0cf9f2be5122eedf0e78bd65d02dc9232db13c4f
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2024-08-26 21:40:34 +0600

    Time: 1 ms (25.92%), Space: 48.3 MB (5.93%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.java
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -1,31 +1,12 @@
 class Solution {
     public int hIndex(int[] citations) {
         int len = citations.length;
-        int l = 0;
-        int r = citations[len - 1];
-        int maxH = 0;
-        while (l <= r) {
-            int mid = l + (r-l)/2;
-            int bound = getUpperBound(0, len-1, mid, citations);
-            if(len - bound >= mid) {
-                maxH = mid;
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
+        int ans = len;
+        for(int i = 0; i < len; i++) {
+           if(citations[i] >= ans ) break;
+           ans--;
         }
-        return maxH;
-       
+        return ans;
     }
-    private int getUpperBound(int l, int r, int num, int[] citations) {
-        while(l < r) {
-            int mid = l + (r - l)/2;
-            if(citations[mid] < num) {
-                l = mid + 1;
-            } else {
-                r = mid;
-            }
-        }
-        return l;
-    }
+    
 }
