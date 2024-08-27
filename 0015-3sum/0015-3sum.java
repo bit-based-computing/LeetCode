bd6dca9e1331a195779211f670d5fc154d981d86
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2024-08-28 05:50:14 +0600

    Time: 35 ms (38.2%), Space: 51.8 MB (42.85%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int len = nums.length;
+        Arrays.sort(nums);
+        for (int i = 0; i < len; i++) {
+            if(i > 0 && nums[i-1] == nums[i]) continue;
+            int l = i+1;
+            int r = len - 1;
+            while (l < r) {
+                if (nums[i] + nums[l] + nums[r] == 0) {
+                    ans.add(new ArrayList<>(Arrays.asList(nums[i], nums[l], nums[r])));
+                   l++;
+                   while(l < r && nums[l] == nums[l-1]) {
+                    l++;
+                   }
+                } else if (nums[i] + nums[l] + nums[r] > 0) {
+                    r--;
+                } else {
+                    l++;
+                }
+            }
+        }
+        return ans;
+    }
+}
