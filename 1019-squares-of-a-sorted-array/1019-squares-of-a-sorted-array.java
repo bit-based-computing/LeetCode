febc3b9d1b908c0cb14c0b3a3369b7f4a1b3bff7
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2024-08-27 06:23:00 +0600

    Time: 1 ms (100%), Space: 48 MB (8.78%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -1,27 +1,18 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
         int len = nums.length;
-        int l = -1;
-        for (int i = 0; i < len; i++) {
-            if(nums[i] < 0) l = i;
-            nums[i] = nums[i] * nums[i];
-            
-        }
-       int[] ans = new int[len];
-       int r = l + 1;
-       int k = 0;
-       while(l >= 0 && r < len) {
-            if(nums[l] <= nums[r]) {
-                ans[k++] = nums[l--];
+        int[] ans = new int[len];
+        int l = 0;
+        int r = len - 1;
+        int k = len - 1;
+       while (l <= r) {
+            if (Math.abs(nums[l]) <= Math.abs(nums[r])) {
+                ans[k--] = nums[r] * nums[r];
+                r--;
             } else {
-                ans[k++] = nums[r++];
+                ans[k--] = nums[l] * nums[l];
+                l++;
             }
-       }
-       while( l >= 0) {
-            ans[k++] = nums[l--];
-       }
-       while (r < len) {
-            ans[k++] = nums[r++];
        }
         return ans;
     }
