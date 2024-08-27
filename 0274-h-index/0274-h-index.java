1d0a0c7c0947d353750cdce34f46d23dbca6368c
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2024-08-27 19:51:14 +0600

    Time: 5 ms (11.4%), Space: 41.7 MB (29.06%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int len = citations.length;
+        int l = 0;
+        int r = len - 1;
+        int maxH = 0;
+        while(l <= r) {
+            int mid = l + (r - l)/2;
+            System.out.println(mid);
+            if(citations[mid] >= len - mid) {
+                maxH = len - mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return maxH;
+    }
+}
