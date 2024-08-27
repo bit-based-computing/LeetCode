f314bbc779033311d545f757cca526f7db1c2ad2
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2024-08-27 06:37:37 +0600

    Time: 0 ms (100%), Space: 44.6 MB (64.3%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,16 +1,12 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+        int k = 0;
         int len = nums.length;
-        int l = 0;
-        int r = l + 1;
-        while (r < len) {
-            if(nums[r] == nums[l]) r++;
-            else {
-                nums[l+1] = nums[r];
-                l++;
-                r++;
+        for(int i = 1; i < len; i++){
+            if(nums[k] != nums[i]){
+                nums[++k] = nums[i];
             }
         }
-        return l + 1;
+        return k + 1;
     }
 }
