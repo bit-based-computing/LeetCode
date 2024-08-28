1d895c15d1dbccd23576a7819500cebccdd4b578
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2024-08-28 06:22:35 +0600

    Time: 33 ms (41.56%), Space: 51.9 MB (42.85%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,18 +3,22 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         int len = nums.length;
         Arrays.sort(nums);
-        for (int i = 0; i < len; i++) {
+        int l, r, total, target;
+        for (int i = 0; i < len-2; i++) {
             if(i > 0 && nums[i-1] == nums[i]) continue;
-            int l = i+1;
-            int r = len - 1;
+             l = i+1;
+             r = len - 1;
+             total = 0;
+             target = -nums[i];
             while (l < r) {
-                if (nums[l] + nums[r] == -nums[i]) {
+                total = nums[l] + nums[r];
+                if (total == target) {
                    ans.add(new ArrayList<>(Arrays.asList(nums[i], nums[l], nums[r])));
                    l++;
                    r--;
                    while (l < r && nums[l] == nums[l-1]) l++;
                    while(r > l && nums[r] == nums[r+1]) r--;
-                } else if (nums[l] + nums[r] > -nums[i]) {
+                } else if (total > target) {
                     r--;
                 } else {
                     l++;
