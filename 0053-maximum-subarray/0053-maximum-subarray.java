95c1b5f11784623334224557ff5e7e918aa50792
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2024-08-28 06:44:16 +0600

    Time: 2 ms (5.83%), Space: 59.6 MB (5.24%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,12 +1,15 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int maxSum = nums[0];
-        int sum = 0;
-        for(int num: nums) {
-            sum += num;
-            if(sum > maxSum) maxSum = sum;
-            if(sum < 0) sum = 0;
-        }
-        return maxSum;
+        int max = -100001;
+		int sum = 0;
+		boolean isAllNegative = true;
+		for(int i=0; i<nums.length; i++){
+			sum += nums[i];
+			if(sum > max) max = sum;
+			if(sum < 0) sum = 0;
+			if(nums[i] > 0) isAllNegative = false;
+		}
+		if(sum > max && !isAllNegative) max = sum;
+		return max;
     }
 }
