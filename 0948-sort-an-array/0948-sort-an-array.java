bd4235eaa7f616bf5a100b08ad945b1aa7b3501d
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2024-08-28 07:00:59 +0600

    Time: 27 ms (47.71%), Space: 57 MB (17.34%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        devide(0, nums.length - 1, nums);
+        return nums;
+    }
+    private void devide(int l, int r, int[] nums) {
+        if(l == r || l > r) return;
+        int mid = l + (r - l)/2;
+        devide(l, mid, nums);
+        devide(mid + 1, r, nums);
+        int i = l;
+        int j = mid + 1;
+        int[] temp = new int[r - l + 1];
+        int k = 0;
+        while (i <= mid && j <= r) {
+            if (nums[i] <= nums[j]) temp[k++] = nums[i++];
+            else temp[k++] = nums[j++];
+        }
+        while (i <= mid) temp[k++] = nums[i++];
+        while (j <= r) temp[k++] = nums[j++];
+        for (i = l; i <= r; i++) nums[i] = temp[i-l];
+        
+
+    }
+}
