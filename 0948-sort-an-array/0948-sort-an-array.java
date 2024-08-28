de153757ab24d5f4e464f186bf841f3a44057959
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2024-08-28 07:06:39 +0600

    Time: 27 ms (47.71%), Space: 57.1 MB (11.48%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -8,6 +8,10 @@ class Solution {
         int mid = l + (r - l)/2;
         devide(l, mid, nums);
         devide(mid + 1, r, nums);
+        merge(l, mid, r, nums);
+    }
+
+    private void merge(int l, int mid, int r, int[] nums) {
         int i = l;
         int j = mid + 1;
         int[] temp = new int[r - l + 1];
@@ -19,7 +23,5 @@ class Solution {
         while (i <= mid) temp[k++] = nums[i++];
         while (j <= r) temp[k++] = nums[j++];
         for (i = l; i <= r; i++) nums[i] = temp[i-l];
-        
-
     }
 }
