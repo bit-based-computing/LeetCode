f1988283651c6ba695a915d735a8ad4c923a3008
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2024-08-31 12:18:32 +0600

    Time: 61 ms (45.01%), Space: 61.4 MB (23.31%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -9,5 +9,30 @@ class Solution {
        }
        return pq.peek();       
     }
+
+    // another better approce 
+
+    /*
+
+    public int findKthLargest(int[] nums, int k) {
+        int[] counts = new int[20001];
+        for(int num : nums) {
+            counts[num + 10000]++;
+        }
+        int ithLargest = 0;
+        int right = 20000;
+        while(right >= 0) {
+            if(counts[right] > 0) {
+                ithLargest += counts[right];
+                if(ithLargest >= k) {
+                    break;
+                }
+            }
+            right--;
+        }
+        return right - 10000;
+    }
+
+    */
    
 }
