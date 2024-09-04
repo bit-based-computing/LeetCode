8205a784f59cbacf13a3d23b161f113119360617
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2024-09-04 07:14:32 +0600

    Time: 0 ms (100%), Space: 42.2 MB (28.53%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -2,26 +2,27 @@ class Solution {
     public void rotate(int[][] matrix) {
         int row = matrix.length;
         int col = matrix[0].length;
-
         int r1 = 0;
-        int c1 = 0;
-        int r2= row - 1;
-        int c2 = col - 1;
+        int r2 = row - 1;
+        int temp;
         while(r1 < r2) {
-            for(int i = c1; i < c2; i++){
-                int a = matrix[r1][i];
-                int b = matrix[i][c2];
-                int c = matrix[r2][c2 - (i-c1)];
-                int d = matrix[r2-(i-c1)][c1];
-                matrix[r1][i] = d;
-                matrix[i][c2] = a;
-                matrix[r2][c2 - (i-c1)] = b;
-                matrix[r2-(i-c1)][c1] = c;
+            for(int i = 0; i < col; i++) {
+                temp = matrix[r1][i];
+                matrix[r1][i] = matrix[r2][i];
+                matrix[r2][i] = temp;
             }
             r1++;
-            c1++;
             r2--;
-            c2--;
         }
+
+        for (int r = 0; r < row; r++) {
+            for(int c = r + 1; c < col; c++) {
+                temp = matrix[r][c];
+                matrix[r][c] = matrix[c][r];
+                matrix[c][r] = temp; 
+            }
+        }
+
+
     }
 }
