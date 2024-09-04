560db06c2072beb6c81a25120fa387b6e13a47f3
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2024-09-04 06:18:45 +0600

    Time: 15 ms (100%), Space: 42.4 MB (17.24%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        int r1 = 0;
+        int c1 = 0;
+        int r2= row - 1;
+        int c2 = col - 1;
+        while(r1 < r2) {
+            for(int i = c1; i < c2; i++){
+                int a = matrix[r1][i];
+                int b = matrix[i][c2];
+                int c = matrix[r2][c2 - (i-c1)];
+                int d = matrix[r2-(i-c1)][c1];
+                System.out.println(a + " " + b + " " + c + " " + d);
+                matrix[r1][i] = d;
+                matrix[i][c2] = a;
+                matrix[r2][c2 - (i-c1)] = b;
+                matrix[r2-(i-c1)][c1] = c;
+            }
+            r1++;
+            c1++;
+            r2--;
+            c2--;
+        }
+    }
+}
