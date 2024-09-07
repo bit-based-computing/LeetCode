15a53682e31c32c9326d513828510309e3fea7a1
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2024-09-08 00:47:47 +0600

    Time: 5 ms (78.15%), Space: 45.6 MB (71.71%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,38 +1,39 @@
 class Solution {
-    public int[] findOrder(int numCourses, int[][] p) {
-        if(numCourses == 1) return new int[]{0};
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
         int[] indegree = new int[numCourses];
-        int[] outdegree = new int[numCourses];
-        Map<Integer, Set<Integer>> map = new HashMap<>();
-        int len = p.length;
-        List<Integer> ans = new ArrayList<>();
-        for(int i = 0; i < len; i++) {
-            indegree[p[i][0]]++;
-            if(map.containsKey(p[i][1])) {
-                map.get(p[i][1]).add(p[i][0]);
-            } else {
-                Set<Integer> set = new HashSet<>();
-                set.add(p[i][0]);
-                map.put(p[i][1], set);
+        List<Integer>[] edges =  new List[numCourses];
+        Queue<Integer> q = new LinkedList<>();
+        int[] fans = new int[numCourses];
+        int a,b;
+        for(int i = 0; i < prerequisites.length; i++){
+            a = prerequisites[i][0];
+            b = prerequisites[i][1];
+            if(edges[b] == null){
+                edges[b] = new ArrayList<Integer>();
+            }
+            indegree[a]++;
+            edges[b].add(a);
+        }
+        for(int i = 0; i < numCourses; i++){
+            if(indegree[i] == 0){
+                indegree[i]--;
+                q.add(i);
             }
         }
-        Queue<Integer> q = new LinkedList<>();
-        for ( int i = 0; i < numCourses; i++) {
-                if(indegree[i] == 0) q.add(i);
-        }
-        while(!q.isEmpty()) {
-            int c = q.poll();
-            ans.add(c);
-            if(map.containsKey(c)) {
-                Set<Integer> s = map.get(c);
-                for(int rc: s) {
-                    indegree[rc]--;
-                    if(indegree[rc] == 0) q.add(rc);
+        int count = 0;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            fans[count] = node;
+            count++;
+            List<Integer> childNodes = edges[node];
+            for(int i = 0; childNodes != null && i < childNodes.size(); i++) {
+                indegree[childNodes.get(i)]--;
+                if(indegree[childNodes.get(i)] == 0){
+                    q.add(childNodes.get(i));
                 }
             }
         }
-     if(ans.size() == numCourses) return ans.stream().mapToInt(Integer::intValue).toArray();
-     return new int[] {};
-
+        if(count == numCourses) return fans;
+        return new int[0];
     }
 }
