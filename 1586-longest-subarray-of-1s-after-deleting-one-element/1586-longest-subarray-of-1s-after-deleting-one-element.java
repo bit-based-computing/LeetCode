79d9031cf245510f5c6c7af8baeeb1cdc36d0ce9
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2024-09-09 16:52:22 +0600

    Time: 2 ms (97.28%), Space: 57.1 MB (26.99%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,24 +1,20 @@
 class Solution {
     public int longestSubarray(int[] nums) {
         List<Integer> list = new ArrayList<>();
+        int psum = 0;
         int sum = 0;
         int ans = 0;
         for(int i = 0; i < nums.length; i++) {
             sum += nums[i];
             if(nums[i] == 0) {
-                    list.add(sum);
-                    sum = 0;
+                  if(psum + sum > ans) ans = psum + sum;
+                  psum = sum;
+                  sum = 0;
             }
         }
-        list.add(sum);
-        if(sum > ans) ans = sum;
-        for(int i = 1; i < list.size(); i++) {
-           sum = list.get(i) + list.get(i-1);
-            if(sum > ans) {
-                ans = sum;
-            }
-        }
-        if(ans == nums.length) ans--;
+        sum = psum + sum;
+        if(sum > ans) ans = sum; 
+        if(sum == nums.length) return sum - 1;
         return ans;
     }
 }
