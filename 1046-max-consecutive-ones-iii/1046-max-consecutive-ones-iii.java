6410d3b2959d3988419d92a935e520b733f4ee72
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2024-09-09 23:50:43 +0600

    Time: 2 ms (100%), Space: 46.8 MB (81.29%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -1,44 +1,19 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        List<Integer> list = new ArrayList<>();
-        int fk = k;
-        int len = nums.length;
-        int sum = 0;
-        for (int i = 0; i < len; i++) {
-            sum += nums[i];
-            if(nums[i] == 0) {
-                list.add(sum);
-                if(sum != 0) list.add(0);
-                sum = 0;
-            }
-        }
-        if(sum != 0) list.add(sum);
         int l = 0;
         int r = 0;
-        int maxSum = 0;
-        sum = 0;
-        while(l <= r && r < list.size()) {
-            int num = list.get(r);
-            if(num == 0) {
-                if(k > 0) k--;
-                else if(k <= 0) {
-                    while(l <= r && list.get(l) != 0) {
-                        sum -= list.get(l);
-                        l++;
-                    }
-                    l++;
-                    k = 0;
-                }
-                
-            } else {
-                sum += num;
-                if(sum > maxSum) {
-                    maxSum = sum;
-                }
+        int zeroCount = 0;
+        while( r < nums.length) {
+            if(nums[r] == 0) {
+                zeroCount++;
             }
             r++;
+            if(zeroCount > k) {
+                if(nums[l] == 0)
+                    zeroCount--;
+                l++;
+            }          
         }
-        maxSum += fk;
-        return Math.min(maxSum, len);
+        return r - l;
     }
 }
