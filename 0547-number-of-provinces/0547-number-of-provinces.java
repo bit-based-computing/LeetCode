c76c0b4ca65704ed33ec4937a13401ed62794b81
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2024-09-12 23:34:24 +0600

    Time: 2 ms (51.81%), Space: 48 MB (27.78%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -3,14 +3,15 @@ class Solution {
         boolean[] visited = new boolean[isConnected.length];
         Queue<Integer> q = new LinkedList<>();
         int count = 0;
-        for( int i = 0; i < isConnected.length; i++) {
+        int len = isConnected.length;
+        for( int i = 0; i < len; i++) {
             if(!visited[i]) {
                 visited[i] = true;
                 count++;
                 q.add(i);
                 while(!q.isEmpty()) {
                     int city = q.poll();
-                    for(int j = 0; j < isConnected.length; j++) {
+                    for(int j = 0; j < len; j++) {
                         if(isConnected[city][j] == 1 && !visited[j]) {
                             visited[j] = true;
                             q.add(j);
