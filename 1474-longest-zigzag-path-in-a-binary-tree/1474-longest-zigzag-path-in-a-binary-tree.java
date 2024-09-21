e6bf44a439cec46aaa5b5cb0e5dbd8777a41e55d
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2024-09-21 21:32:26 +0600

    Time: 3 ms (99.68%), Space: 54.5 MB (73.86%) - LeetHub

---
+++ 1474-longest-zigzag-path-in-a-binary-tree/1474-longest-zigzag-path-in-a-binary-tree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int ans = 0;
+    public int longestZigZag(TreeNode root) {
+        getAns(1, 1, root.right);
+        getAns(1, 0, root.left);
+        return ans;
+    }
+
+    private void getAns(int count, int side, TreeNode root) {
+         if(root == null) return;
+        ans = Math.max(ans, count);
+        if(side == 1) {
+            getAns(1, 1, root.right);
+            getAns(count + 1, 0, root.left);
+        } else {
+            getAns(count + 1, 1, root.right);
+            getAns(1, 0, root.left);
+        }
+    }
+}
