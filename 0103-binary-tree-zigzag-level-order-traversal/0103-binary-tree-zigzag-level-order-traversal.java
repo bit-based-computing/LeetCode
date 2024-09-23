692051f9763ecb63bd3e382c6c70009f1d488004
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2024-09-23 23:27:15 +0600

    Time: 0 ms (100%), Space: 42 MB (61.6%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -22,8 +22,7 @@ class Solution {
 
     private void addNodes(TreeNode root, List<List<Integer>> list, int level) {
         if(root == null) return ;
-        if(level >= list.size())
-            list.add(new ArrayList<Integer>());
+        if(level >= list.size()) list.add(new ArrayList<Integer>());
         if(level % 2 == 0) list.get(level).add(root.val);
         else list.get(level).add(0, root.val);
         addNodes(root.left, list, level + 1);
