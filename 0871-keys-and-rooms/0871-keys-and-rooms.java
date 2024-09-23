daa398cf17e837239baa600c202a06dcced1dec8
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2024-09-23 11:06:42 +0600

    Time: 4 ms (12.15%), Space: 43.9 MB (87.8%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.java
+++ 0871-keys-and-rooms/0871-keys-and-rooms.java
@@ -1,15 +1,17 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean[] visit = new boolean[1001];
         Set<Integer> set = new HashSet<>();
         Queue<Integer> q = new LinkedList<>();
         q.add(0);
         while(!q.isEmpty()) {
             int room = q.poll();
+            visit[room] = true;
             set.add(room);
             List<Integer>keys = rooms.get(room);
             for(int i = 0; i < keys.size(); i++) {
                 int key = keys.get(i);
-                if(!set.contains(key)) {
+                if(!visit[key]) {
                     q.add(key);
                 }
             }
