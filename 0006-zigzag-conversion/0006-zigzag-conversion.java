64bbb3e7eca670aded3f3067944dd5693a3bd049
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-02-09 00:21:15 +0600

    Time: 7 ms (32.63%), Space: 45.9 MB (14.03%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows == 1) return s;
+        int slen = s.length();
+        char[] sc = s.toCharArray();
+        StringBuilder[] sb = new StringBuilder[numRows];
+        for(int i = 0; i < numRows; i++) {
+            sb[i] = new StringBuilder();
+        }
+        boolean down = true;
+        int count = 0;
+        for(int i = 0; i < slen; i++) {
+            sb[count].append(sc[i]);
+            if(down) {
+                if(count == numRows - 1) {
+                    count--;
+                    down = !down;
+                } else {
+                    count++;
+                }
+            } else {
+                if(count == 0) {
+                    count++;
+                    down = !down;
+                } else {
+                    count--;
+                }
+            }
+        }
+        String ans = "";
+        for(int i = 0; i < numRows; i++) {
+            ans = ans + sb[i].toString();
+        }
+        return ans;
+    }
+}
