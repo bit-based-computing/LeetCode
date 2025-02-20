3bc2ff6baca119cc37278e5343fb0959007af7f4
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-02-20 22:05:26 +0600

    Time: 2 ms (99.37%), Space: 49.4 MB (52.37%) - LeetHub

--- 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.java
+++ 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.java
@@ -1,22 +1,28 @@
 class Solution {
-    public int maxSubarraySumCircular(int[] nums) {
-        int mostNegative = 0;
+    public int findSum(int[] nums, boolean isMax) {
         int ans = nums[0];
         int sum = 0;
-        int negativeSum = 0;
-        int allSum = 0;
-        boolean isAllNegative = true;
-        for(int num: nums) {
-            allSum += num;
-            sum += num;
-            negativeSum = negativeSum + num;
-            if(sum > ans) ans = sum;
-            if(negativeSum < mostNegative) mostNegative = negativeSum;
-            if(sum < 0) sum = 0;
-            if(negativeSum > 0) negativeSum = 0;
-            if(num > 0) isAllNegative = false;
+        if(isMax) {
+           for(int num: nums) {
+                sum += num;
+                if(sum > ans) ans = sum;
+                if(sum < 0) sum = 0;
+           }
+        } else {
+            for(int num: nums) {
+                sum += num;
+                if(sum < ans) ans = sum;
+                if(sum > 0) sum = 0;
+           }
         }
-        if(isAllNegative) return ans;
-        return Math.max(ans, allSum - mostNegative);
+        return ans;
+    }
+    public int maxSubarraySumCircular(int[] nums) {
+        int max = findSum(nums, true);
+        int min = findSum(nums, false);
+        int total = 0;
+        for(int num: nums) total+=num;
+        if(max > 0) return Math.max(max, total - min);
+        return max;
     }
 }
