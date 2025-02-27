a709c92a0d35a4d63b7ca4413d59d2f1451736ff
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-02-27 10:42:18 +0600

    Time: 3 ms (6.82%), Space: 58.3 MB (39.91%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int start = 0;
+        int sum = 0;
+        int ans = nums.length + 1;
+        int minLength;
+        for(int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            while(start < nums.length && sum-nums[start] >= target) {
+                sum -= nums[start++];
+            }
+            if(sum >= target) {
+               minLength = i - start + 1;
+               if(minLength < ans) ans = minLength;
+            }
+        }
+        if(ans > nums.length) return 0;
+        return ans;
+    }
+}
