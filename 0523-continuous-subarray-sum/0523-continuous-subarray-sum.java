0d765e13b40daf73835d00fa763ab1c7001dc563
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-03-02 02:48:19 +0600

    Time: 22 ms (97.13%), Space: 57.4 MB (84.64%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -7,8 +7,8 @@ class Solution {
             sum += nums[i];
             rem = sum % k;
             if(rem == 0 && i > 0) return true;
-            if(map.get(rem) == null) map.put(rem, i + 1);
-            if(i - map.get(rem) > 0) return true;
+            if(map.get(rem) == null) map.put(rem, i);
+            else if(i - map.get(rem) > 1) return true;
         }
         return false;
     }
