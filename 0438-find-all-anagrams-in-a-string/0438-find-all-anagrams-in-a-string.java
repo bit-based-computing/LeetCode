4851cf48ab4b5423c8fa6b0c4badb14257f3f4fe
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-03-03 00:23:18 +0600

    Time: 7 ms (95.89%), Space: 44.6 MB (96%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,41 +1,23 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> ans = new ArrayList<>();
-        int lens = s.length();
-        int lenp = p.length();
-        int sum = lenp;
-        int[] freq = new int[26];
-        for (int i = 0; i < lenp; i++)
-            freq[p.charAt(i) - 'a']++;
-
-        for (int i = 0; i < 26; i++)
-            if (freq[i] == 0)
-                freq[i] = -1;
-        int wsize = lenp - 1;
-        int start = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (i - start > wsize) {
-                if (freq[s.charAt(start) - 'a'] >= 0) {
-                    freq[s.charAt(start) - 'a']++;
-                    sum++;
-                }
-                start++;
+        int slen = s.length();
+        int plen = p.length();
+        if(slen < plen) return ans;
+        int[] sfreq = new int[26];
+        int[] pfreq = new int[26];
+        char[] sc = s.toCharArray();
+        char[] pc = p.toCharArray();
+        for(int i = 0; i < plen; i++) {
+            pfreq[pc[i] - 'a']++;
+            sfreq[sc[i] - 'a']++;
+        }
+        for(int i = 0; i <= slen - plen; i++) {
+            if(Arrays.equals(pfreq, sfreq)) ans.add(i);
+            sfreq[sc[i] - 'a']--;
+            if(i + plen < slen) {
+                sfreq[sc[i+plen] - 'a']++;
             }
-            if (freq[s.charAt(i) - 'a'] > 0) {
-                freq[s.charAt(i) - 'a']--;
-                sum--;
-            } else if (freq[s.charAt(i) - 'a'] == 0) {
-                while (s.charAt(start) != s.charAt(i)) {
-                    if (freq[s.charAt(start) - 'a'] >= 0) {
-                        freq[s.charAt(start) - 'a']++;
-                        sum++;
-                    }
-                    start++;
-                }
-                start++;
-            }
-            if (sum == 0)
-                ans.add(start);
         }
         return ans;
     }
