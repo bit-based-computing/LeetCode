e56a1ea314864719f450a3c3f06c8ae8cc49d572
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-03-06 00:09:02 +0600

    Time: 0 ms (100%), Space: 41.8 MB (15.57%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,45 +1,52 @@
 class Solution {
+    int row = 0;
+    int col = 0;
+    void spiralMove(int[][] matrix, int i, int j, List<Integer> ans, String moveTo) {
+        int oi = i;
+        int oj = j;
+        if(moveTo.equals("forward")) {
+            if(j + 1 <= col && matrix[i][j+1] != -101) {
+                j++;
+            } else if(i + 1 <= row && matrix[i+1][j] != -101){
+                i++;
+                moveTo = "down";
+            }
+        } else if(moveTo.equals("down")) {
+            if(i + 1 <= row && matrix[i+1][j] != -101) {
+                i++;
+            } else if(j - 1 >= 0 && matrix[i][j - 1] != -101){
+                j--;
+                moveTo = "backward";
+            }
+        } else if(moveTo.equals("backward")) {
+            if(j - 1 >= 0 && matrix[i][j-1] != -101) {
+                j--;
+            } else if(i - 1 >= 0 && matrix[i - 1][j] != -101){
+                i--;
+                moveTo = "up";
+            }
+        } else if(moveTo.equals("up")) {
+            if(i - 1 >= 0 && matrix[i-1][j] != -101) {
+                i--;
+            } else if(j + 1 <= col && matrix[i][j+1] != -101){
+                j++;
+                moveTo = "forward";
+            }
+        }
+        if(oi != i || oj != j) {
+            ans.add(matrix[i][j]);
+            matrix[i][j] = -101;
+            spiralMove(matrix, i, j, ans, moveTo);
+        }
+        
+    }
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> ans = new ArrayList<>();
-        int row = matrix.length-1;
-        int col = matrix[0].length-1;
-
-        int r = 0;
-        while(r <= row/2) {
-
-            //--->
-            for(int c = r; c <= col - r; c++) {
-                ans.add(matrix[r][c]);
-            }
-
-            /*
-                |
-                |
-                >
-
-            */
-            for(int i = r+1; i < row - r && col-r >= r ; i++) {
-                ans.add(matrix[i][col - r]);
-            }
-
-            // <---
-            for(int c = col - r; row - r > r  && c >= r; c--){
-                ans.add(matrix[row-r][c]);
-            }
-
-
-            /*
-            >
-            |
-            |
-            */
-
-            for(int i = row-r-1; i > r && r < col-r; i--) {
-                ans.add(matrix[i][r]);
-            }
-
-            r++;
-        }
+        row = matrix.length - 1;
+        col = matrix[0].length - 1;
+        ans.add(matrix[0][0]);
+        matrix[0][0] = -101;
+        spiralMove(matrix, 0, 0, ans, "forward");
         return ans;
     }
 }
