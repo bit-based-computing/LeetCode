9dab35a2f397f257fd13a44de7f8767733c7dbf7
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-03-07 00:58:47 +0600

    Time: 1 ms (100%), Space: 44.3 MB (78.15%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,66 +1,62 @@
 class Solution {
     void setZero(int[] freq) {
-        for(int i = 0; i < 9; i++)
+        for (int i = 0; i < 9; i++)
             freq[i] = 0;
     }
+
+    boolean isValid(int col, char[][] board, int[] freq) {
+        int index = 0;
+        for (int i = 0; i < 9; i++) {
+            if(i % 3 == 0) setZero(freq);
+            for (int j = col; j < col + 3; j++) {
+                if (board[j][i] != '.') {
+                    index = board[j][i] - '1';
+                    if (freq[index] > 0)
+                        return false;
+                    freq[index]++;
+                }
+            }
+        }
+        return true;
+    }
+
     public boolean isValidSudoku(char[][] board) {
         int[] freq = new int[9];
         int index = 0;
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) {
-                if(board[i][j] != '.') {
+
+        //check rows
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
                     index = board[i][j] - '1';
-                    if(freq[index] > 0) return false;
+                    if (freq[index] > 0)
+                        return false;
                     freq[index]++;
                 }
             }
             setZero(freq);
         }
 
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) {
-                if(board[j][i] != '.') {
+        // check column
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[j][i] != '.') {
                     index = board[j][i] - '1';
-                    if(freq[index] > 0) return false;
+                    if (freq[index] > 0)
+                        return false;
                     freq[index]++;
                 }
             }
             setZero(freq);
         }
 
-        for(int i = 0; i < 9; i++) {
-            if(i % 3 == 0) setZero(freq);
-            for(int j = 0; j < 3; j++) {
-                if(board[i][j] != '.') {
-                    index = board[i][j] - '1';
-                    if(freq[index] > 0) return false;
-                    freq[index]++;
-                }
-            }
-        }
-
-        for(int i = 0; i < 9; i++) {
-            if(i % 3 == 0) setZero(freq);
-            for(int j = 3; j < 6; j++) {
-                if(board[i][j] != '.') {
-                    index = board[i][j] - '1';
-                    if(freq[index] > 0) return false;
-                    freq[index]++;
-                }
-            }
-        }
-
-        for(int i = 0; i < 9; i++) {
-            if(i % 3 == 0) setZero(freq);
-            for(int j = 6; j < 9; j++) {
-                if(board[i][j] != '.') {
-                    index = board[i][j] - '1';
-                    if(freq[index] > 0) return false;
-                    freq[index]++;
-                }
-            }
-        }
-        
+        //check 3 * 3 grids
+        if (!isValid(0, board, freq))
+            return false;
+        if (!isValid(3, board, freq))
+            return false;
+        if (!isValid(6, board, freq))
+            return false;
         return true;
     }
 }
