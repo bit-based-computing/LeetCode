327420b577705cad6af4aad7731a95d7930f4f44
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-03-07 15:21:58 +0600

    Time: 0 ms (100%), Space: 41.5 MB (85.45%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.java
+++ 0289-game-of-life/0289-game-of-life.java
@@ -1,39 +1,41 @@
 class Solution {
-    int[] dr = {-1, -1, -1, 0, 1, 1, 1, 0,};
-    int[] dc = {-1, 0, 1, 1, 1, 0, -1, -1};
+    boolean isValid(int i, int j, int row, int col) {
+        return i >= 0 && i < row && j >= 0 && j < col;
+    }
     public void gameOfLife(int[][] board) {
+        int[]dx = {0, 1, 1, 1, 0, -1, -1, -1};
+        int[]dy = {-1,-1, 0, 1, 1, 1, 0, -1};
         int row = board.length;
         int col = board[0].length;
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                int liveCell = 0;
-                for (int k = 0; k < 8; k++) {
-                    int r = i + dr[k];
-                    int c = j + dc[k];
-                    if (isValid(r, c, row, col)) {
-                        if (Math.abs(board[r][c]) == 1) {
-                            liveCell++;
-                        }
+        int x = 0;
+        int y = 0;
+        int liveCount = 0;
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                liveCount = 0;
+                for(int k = 0; k < 8; k++) {
+                    x = i + dx[k];
+                    y = j + dy[k];
+                    if(isValid(x,y,row,col) && Math.abs(board[x][y]) == 1){
+                        liveCount++;
                     }
                 }
-                if ((liveCell < 2 || liveCell > 3) && board[i][j] == 1) {
-                    board[i][j] = board[i][j] * -1;
-                } else if(liveCell == 3 && board[i][j] == 0) {
+                if(board[i][j] == 1 && liveCount < 2) {
+                    board[i][j] = -1;
+                } else if(board[i][j] == 1 && liveCount > 3){
+                    board[i][j] = -1;
+                } else if(board[i][j] == 0 && liveCount == 3){
                     board[i][j] = 2;
                 }
             }
         }
 
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
                 if(board[i][j] == -1) board[i][j] = 0;
                 else if(board[i][j] == 2) board[i][j] = 1;
             }
         }
-    }
-   
-    private boolean isValid(int i, int j, int row, int col) {
-        if(i < 0 || i >= row || j < 0 || j >= col) return false;
-        return true;
+
     }
 }
