d982796b9efca6110910651f0d4f22efc27597e4
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-03-11 11:33:22 +0600

    Time: 7 ms (86.45%), Space: 45.6 MB (68.73%) - LeetHub

--- 0838-design-linked-list/0838-design-linked-list.java
+++ 0838-design-linked-list/0838-design-linked-list.java
@@ -1,9 +1,12 @@
 class Node {
     int val;
     Node next;
-
-    public Node(){
-
+    public Node(int val) {
+        this.val = val;
+    }
+    public Node(int val, Node next) {
+        this.val = val;
+        this.next = next;
     }
 }
 
@@ -11,86 +14,67 @@ class MyLinkedList {
 
     Node head;
     Node tail;
+    int size;
 
     public MyLinkedList() {
-        
+        head = tail = null;
+        size = 0;
     }
     
     public int get(int index) {
-        int count = 0;
+        if(index < 0 || index >= size) return -1;
         Node temp = head;
-        while(temp != null) {
-            if(count == index){
-                return temp.val;
-            }
-            temp = temp.next;
-            count++;
-        }
-        return -1;
+        for(int i = 0; i < index; i++) temp = temp.next;
+        return temp.val;
     }
     
     public void addAtHead(int val) {
-        Node currentHead = new Node();
-        currentHead.val = val;
-        currentHead.next = head;
-        head = currentHead;
+        Node node = new Node(val, head);
+        head = node;
+        if(size == 0) tail = head;
+        size++;
     }
     
     public void addAtTail(int val) {
-        if(head == null) {
-            addAtIndex(0, val);
-            return;
+        Node node = new Node(val);
+        if(size == 0) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = node;
         }
-        Node node = new Node();
-        node.val = val;
-        Node temp = head;
-        while(temp != null && temp.next != null) {
-            temp = temp.next;
-        }
-        temp.next = node;
+        size++;
     }
     
     public void addAtIndex(int index, int val) {
-        if(index == 0) {
-            Node newNode = new Node();
-            newNode.val = val;
-            newNode.next = head;
-            head = newNode;
-            return;
-        }
-        int count = 0;
-        Node temp = head;
-        while(temp != null) {
-            if(count == index - 1){
-                Node newNode = new Node();
-                newNode.val = val;
-                newNode.next = temp.next;
-                temp.next = newNode;
-                break;
-            }
-            temp = temp.next;
-            count++;
+        if(index < 0 || index > size) return;
+        if(index == 0) addAtHead(val);
+        else if(index == size) addAtTail(val);
+        else {
+            int count = 0;
+            Node newNode = new Node(val);
+            Node temp = head;
+            for(int i = 0; i < index - 1; i++) temp = temp.next;
+            newNode.next = temp.next;
+            temp.next = newNode;
+            size++;
         }
     }
     
     public void deleteAtIndex(int index) {
+        if(index < 0 || index >= size) return;
         if(index == 0) {
-            if(head != null) {
-                head = head.next;
+            head = head.next;
+            if(--size == 0) {
+                tail = null;
             }
-            return;
-        }
-        int count = 0;
-        Node temp = head;
-        while(temp != null) {
-            if(count == index - 1){
-                if(temp.next != null) {
-                    temp.next = temp.next.next;
-                }
-                break;
-            }
-            temp = temp.next;
-            count++;
+        } else {
+            Node temp = head;
+            for(int i = 0; i < index - 1; i++) temp = temp.next;
+            if(temp.next == tail) tail = temp;
+            temp.next = temp.next.next;
+            size--;
         }
     }
 }
