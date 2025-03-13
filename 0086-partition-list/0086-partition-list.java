914bef80652fedefb9073f3a88a66af3a8cd45e3
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-03-13 23:21:03 +0600

    Time: 0 ms (100%), Space: 42.1 MB (69.75%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -1,31 +1,45 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        ListNode before = new ListNode(0);
-        ListNode after = new ListNode(0);
-        ListNode before_cur = before;
-        ListNode after_cur = after;
+        if(head == null) return null;
+        ListNode newHead = null;
+        ListNode newTail = null;
+        ListNode tempH = null;
+        ListNode tempT = null;
         while(head != null) {
             if(head.val < x) {
-                before_cur.next = head;
-                before_cur = before_cur.next;
+                if(newHead == null) {
+                    newHead = head;
+                    tempH = newHead;
+                } else {
+                    tempH.next = head;
+                    tempH = tempH.next;
+                }
             } else {
-                after_cur.next = head;
-                after_cur = after_cur.next;
+                if(newTail == null) {
+                    newTail = head;
+                    tempT = newTail;
+                } else {
+                    tempT.next = head;
+                    tempT = tempT.next;
+                }
             }
             head = head.next;
         }
-        after_cur.next = null;
-        before_cur.next = after.next;
-        return before.next;
+        if(tempT != null) tempT.next = null;
+        if(newHead == null) {
+            return newTail;
+        }
+        tempH.next = newTail;
+        return newHead;
     }
 }
