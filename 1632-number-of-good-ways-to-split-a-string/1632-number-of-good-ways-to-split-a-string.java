6d305988a81432965fa7a1897cb9af9a6d2bd67c
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-03-13 16:50:07 +0600

    Time: 5 ms (96.08%), Space: 45.3 MB (33.77%) - LeetHub

--- 1632-number-of-good-ways-to-split-a-string/1632-number-of-good-ways-to-split-a-string.java
+++ 1632-number-of-good-ways-to-split-a-string/1632-number-of-good-ways-to-split-a-string.java
@@ -12,19 +12,15 @@ class Solution {
         int[] prefix = new int[len];
         int[] suffix = new int[len];
         int index = 0;
-        int count = 1;
-        prefix[0] = 1;
-        freq[cs[0]-'a'] = 1;
-        for(int i = 1; i < len; i++) {
+        int count = 0;
+        for(int i = 0; i < len; i++) {
             index = cs[i] - 'a';
             if(freq[index] == 0) count++;
             freq[index]++;
             prefix[i] = count;
         }
         setZero(freq);
-        count = 1;
-        freq[cs[len-1] - 'a'] = 1;
-        suffix[len-1] = 1;
+        count = 0;
         for(int i = len - 1; i >= 0; i--) {
             index = cs[i] - 'a';
             if(freq[index] == 0) count++;
