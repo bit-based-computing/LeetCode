c3e9677c07ee4953f805a0bdf48feae58edf59af
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-03-16 16:49:08 +0600

    Time: 0 ms (100%), Space: 44.4 MB (57.7%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -1,31 +1,55 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
+
+    ListNode reverse(ListNode start, ListNode end) {
+        ListNode prev = start;
+        ListNode temp = start.next;
+        ListNode next = null;
+        while(temp != end) {
+            next = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = next;
+        }
+        temp.next = prev;
+        return temp;
+    }
+
     public ListNode reverseKGroup(ListNode head, int k) {
-        if(head == null || k == 1) return head;
-        ListNode curh = head;
-        int count = k;
-        while(curh != null && count > 0) {
-            count--;
-            curh = curh.next;
+        if(head == null || head.next == null || k == 1) return head;
+        ListNode temp = head;
+        int count = 0;
+        ListNode ans = null;
+        ListNode start = head;
+        ListNode next = null;
+        ListNode lastReversedNode = start;
+        while(temp != null) {
+              count++;
+              if(count % k == 0) {
+                next = temp.next;
+                ListNode revNode = reverse(start, temp);
+                if(ans == null) {
+                    ans = revNode;
+                } else {
+                    lastReversedNode.next = revNode;
+                }
+                lastReversedNode = start;
+                start = next;
+                temp = next;
+              } else {
+                temp = temp.next;
+              }
         }
-        if(count > 0) return head;
-        ListNode prev = reverseKGroup(curh, k);
-        curh = head;
-        for(int i = 0; i < k; i++) {
-            ListNode next = curh.next;
-            curh.next = prev;
-            prev = curh;
-            curh = next;
-        }
-        return prev;
+        lastReversedNode.next = start;
+        return ans;
     }
 }
