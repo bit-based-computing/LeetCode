a7aa4ae5213c959aba530d8f2f94651b0abb4dea
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-03-16 15:42:11 +0600

    Time: 4 ms (43.63%), Space: 43.2 MB (45.46%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
@@ -1,13 +1,30 @@
 public class Codec {
 
+    private final String baseUrl = "http://tinyurl.com/";
+    private final Map<String, String> map = new HashMap<>();
+    private final String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private final Random rand = new Random();
+    private final int keyLenth = 6;
+
+
     // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
-        return longUrl;
+        String key;
+        do{
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0 ; i < keyLenth; i++) {
+                sb.append(characters.charAt(rand.nextInt(characters.length())));
+            }
+            key = sb.toString();
+        } while(map.containsKey(key) && !map.get(key).equals(longUrl));
+        map.put(key, longUrl);
+        return baseUrl + key;
     }
 
     // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        return shortUrl;
+        String key = shortUrl.replace(baseUrl, "");
+        return map.get(key);
     }
 }
 
