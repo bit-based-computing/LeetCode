24181fcfdbaa99b4f2314ad863e42f9adf8c3afd
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-03-19 16:07:48 +0600

    Time: 40 ms (66.52%), Space: 76.1 MB (31.81%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.java
+++ 0456-132-pattern/0456-132-pattern.java
@@ -1,21 +1,16 @@
 class Solution {
     public boolean find132pattern(int[] nums) {
-        int len = nums.length;
-        int[] min = new int[len];
-        min[0] = nums[0];
-        for (int i = 1; i < len; i++) min[i] = Math.min(nums[i], min[i-1]);
         
-        Stack<Integer> st = new Stack<>();
+        Integer third = Integer.MIN_VALUE;
+        Stack<Integer> stack = new Stack<>();
+        for(int i = nums.length - 1 ; i >= 0; i--) {
+            if(nums[i] < third) return true;
 
-        for (int i = len - 1; i >= 1; i--) {
-            int m = nums[i];
-            if(m > min[i-1]) {
-                while(!st.isEmpty() && st.peek() <= min[i-1]) st.pop();
-                if(!st.isEmpty() && st.peek() < m) return true;
-                st.push(m);
+            while(!stack.isEmpty() && stack.peek() < nums[i]) {
+                third = stack.pop();
             }
+            stack.add(nums[i]);
         }
-
         return false;
     }
 }
