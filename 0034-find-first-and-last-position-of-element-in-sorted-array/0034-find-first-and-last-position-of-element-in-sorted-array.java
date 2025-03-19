582eca6ec289ec6ce63f528b4717c10539acae38
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-03-19 17:01:50 +0600

    Time: 0 ms (100%), Space: 46 MB (47.3%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -0,0 +1,66 @@
+class Solution {
+    public int upperBound(int nums[], int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        int mid = 0;
+        int ans = 0;
+        while (l <= r) {
+            mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
+                l = mid + 1;
+            }
+            else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    public int lowerBound(int nums[], int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        int mid = 0;
+        int ans = 0;
+        while (l <= r) {
+            mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
+                r = mid - 1;
+            }
+            else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+    public boolean isFound(int nums[], int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        int mid = 0;
+        while (l <= r) {
+            mid = l + (r - l) / 2;
+            if (nums[mid] == target)
+                return true;
+            else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return false;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        if (!isFound(nums, target))
+            return new int[] { -1, -1 };
+
+        int lowerBound = lowerBound(nums, target);
+        int upperBound = upperBound(nums, target);
+        return new int[] {lowerBound, upperBound};
+    }
+}
