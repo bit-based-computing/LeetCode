aa22a083999ce11d824bc9f613b1c97fec739e5a
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-03-21 00:19:13 +0600

    Time: 0 ms (100%), Space: 43.3 MB (8.65%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -1,55 +1,31 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1 == null) return list2;
-        if(list2 == null) return list1;
-        ListNode head;
-        if(list1.val < list2.val) {
-            head = list1;
-            list1 = list1.next;
-        } else {
-            head = list2;
-            list2 = list2.next;
-        }
-        ListNode temp = head;
-        ListNode next = null;
-        while(list1 != null && list2 != null) {
-            if(list1.val <= list2.val) {
-                next = list1.next;
-                temp.next = list1;
-                list1 = next;
+        ListNode head = new ListNode(-1);
+        ListNode mergedList = head;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                mergedList.next = list1;
+                list1 = list1.next;
             } else {
-                next = list2.next;
-                temp.next = list2;
-                list2 = next;
+                mergedList.next = list2;
+                list2 = list2.next;
             }
-            temp = temp.next;
+            mergedList = mergedList.next;
         }
 
-        while(list1 != null) {
-             next = list1.next;
-             temp.next = list1;
-             list1 = next;
-             temp = temp.next;
-        }
+       if(list1 == null) mergedList.next = list2;
+       if(list2 == null) mergedList.next = list1;
 
-        while(list2 != null) {
-             next = list2.next;
-             temp.next = list2;
-             list2 = next;
-             temp = temp.next;
-        }
-
-        return head;
-
+       return head.next;
     }
 }
