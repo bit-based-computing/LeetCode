88948a22145810be708eacd7eaca4a6ba6acdc72
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-03-22 13:33:57 +0600

    Time: 26 ms (95.38%), Space: 65.2 MB (5.22%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -2,12 +2,26 @@ class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int len = nums.length;
         int[] result = new int[len - k + 1];
-        Deque<Integer> deque = new ArrayDeque<Integer>();
-        for (int i = 0; i < len; i++) {
-            while (deque.size() > 0 && deque.peekFirst() < i- k + 1) deque.pollFirst();
-            while (deque.size() > 0 && nums[deque.peekLast()] < nums[i]) deque.pollLast();
-            deque.offerLast(i);
-            if (i-k+1>=0) result[i-k+1] = nums[deque.peekFirst()];
+        LinkedList<Integer> max = new LinkedList<>();
+        for(int i = 0; i < k; i++) {
+            while(!max.isEmpty() && nums[max.getLast()] < nums[i]) {
+                max.removeLast();
+            }
+            max.addLast(i);
+        }
+        result[0] = nums[max.getFirst()];
+
+        int l = 0;
+        int r = k - 1;
+        while( r < len - 1) {
+            l++;
+            r++;
+            while(!max.isEmpty() && nums[max.getLast()] < nums[r]) {
+                max.removeLast();
+            }
+            max.addLast(r);
+            if(max.getFirst() < l) max.removeFirst();
+            result[l] = nums[max.getFirst()];
         }
         return result;
     }
