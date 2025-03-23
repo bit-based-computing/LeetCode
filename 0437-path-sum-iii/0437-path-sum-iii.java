b5b51aff6e8df28371199549a4469ccda38644b2
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-03-23 22:31:25 +0600

    Time: 3 ms (96.52%), Space: 44.8 MB (10.64%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -14,26 +14,21 @@
  * }
  */
 class Solution {
-    int count = 0;
     public int pathSum(TreeNode root, int targetSum) {
-        countPath(root, 0, targetSum, false);
-        return count;
+        Map<Long, Integer> prefixSumCount = new HashMap<>();
+        prefixSumCount.put(0L, 1);
+        return countPathSum(root, 0L, targetSum, prefixSumCount);
     }
 
-    public void countPath(TreeNode root, long currentSum, long targetSum, boolean with) {
-        if(root == null) return;
-        if(currentSum + root.val == targetSum){
-            count++;
-        }
-        if(with) {
-            countPath(root.left, currentSum + root.val, targetSum, true);
-            countPath(root.right, currentSum + root.val, targetSum, true);
-        } else {
-            countPath(root.left, currentSum + root.val, targetSum, true);
-            countPath(root.right, currentSum + root.val, targetSum, true);
-            countPath(root.left, 0, targetSum, false);
-            countPath(root.right, 0, targetSum, false);
-        }
-        
+    public int countPathSum(TreeNode node, long currentSum, int targetSum, Map<Long, Integer> prefixSumCount) {
+        if(node == null) return 0;
+        int count = 0;
+        currentSum += node.val;
+        count = prefixSumCount.getOrDefault(currentSum - targetSum, 0);
+        prefixSumCount.put(currentSum, prefixSumCount.getOrDefault(currentSum, 0) + 1);
+        count = count + countPathSum(node.left, currentSum, targetSum, prefixSumCount);
+        count = count + countPathSum(node.right, currentSum, targetSum, prefixSumCount);
+        prefixSumCount.put(currentSum, prefixSumCount.getOrDefault(currentSum, 0) - 1);
+        return count;
     }
 }
