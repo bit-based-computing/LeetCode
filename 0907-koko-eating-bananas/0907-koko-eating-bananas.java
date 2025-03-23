2a8924abed6b37c9fc5c842aaaf9ab3e2c79d734
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-03-23 23:32:15 +0600

    Time: 19 ms (56.38%), Space: 45.8 MB (5.63%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -0,0 +1,27 @@
+class Solution {
+    boolean isValid(int[] piles, long k, long h) {
+        long count = 0;
+        for(long pile: piles) {
+            count = count + (pile/k);
+            if(pile % k != 0) count++;
+        }
+        return count <= h;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        long sum = 0;
+        for(int pile: piles) sum += pile;
+        long ans = 0;
+        long l = 1;
+        long r = sum;
+        while(l <= r) {
+            long mid = l + (r - l)/2;
+            if(isValid(piles, (int)mid, h)) {
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return (int)ans;
+    }
+}
