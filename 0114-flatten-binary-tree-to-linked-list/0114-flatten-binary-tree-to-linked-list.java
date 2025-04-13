babd85bfea4fd4f63f26b4514c12f2f1a6514b36
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-04-13 21:09:47 +0600

    Time: 0 ms (100%), Space: 42.1 MB (65.42%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -14,36 +14,16 @@
  * }
  */
 class Solution {
-
-    // first set all left then process right
-
-    TreeNode cuRoot = null;
+    TreeNode prev = null;
     public void flatten(TreeNode root) {
         if(root == null) return;
-        cuRoot = root;
-        TreeNode right = root.right;
-        if(root.left != null) {
-            root.right = root.left;
-            flatten(root.left);
-            root.left = null;
-        }
-        if(right != null) {
-            cuRoot.right = right;
-            flatten(right);
-        }
-    }
-
-    /* another way 
-        // first set all right then process left
-        TreeNode prev = null;
-        public void flatten(TreeNode root) {
-        if(root == null) return;
-        flatten(root.right);
-        flatten(root.left);
-        root.right = prev;
-        root.left = null;
         prev = root;
+        TreeNode right = root.right;
+        TreeNode left = root.left;
+        root.right = left;
+        root.left = null;
+        flatten(left);
+        prev.right = right;
+        flatten(right);
     }
-
-    */
 }
