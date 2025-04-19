1c594455f443a20227cfd7e09d84209f97ef704b
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-04-19 09:21:19 +0600

    Time: 1 ms (4.46%), Space: 43.3 MB (15.94%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -2,16 +2,25 @@ class Solution {
     public double pow(double x, long n) {
         if(n == 0) return 1;
         if(n % 2 == 0) {
-           return pow(x*x, n/2);
+           x = pow(x, n/2);
+           return x * x;
         } 
         return x * pow(x, n-1);
     }
     public double myPow(double x, int n) {
         long nn = getAbs(n);
-        System.out.println(nn);
-        double ans = pow(x, nn);
-        if(n < 0) return 1/ ans;
-        return ans;
+        double sum = 1.0;
+        while(nn > 0) {
+            if(nn % 2 == 0) {
+                x = x * x;
+                nn = nn / 2;
+            } else {
+                sum = sum * x;
+                nn = nn - 1;
+            }
+        }
+        if(n >= 0) return sum;
+        return 1.0/sum;
     }
     public long getAbs(long n) {
         return Math.abs(n);
