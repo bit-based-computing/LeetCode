4595b454c52bd231a44520d7aa78a29d148cc8a3
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-04-19 19:24:14 +0600

    Time: 2 ms (57.3%), Space: 44.4 MB (67.01%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -15,15 +15,11 @@
  */
 class Solution {
     Map<Integer, Integer> inmap;
-        Map<Integer, Integer> postmap;
-
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         inmap = new HashMap<>();
-        postmap = new HashMap<>();
         for(int i = 0; i < inorder.length; i++) {
             inmap.put(inorder[i], i);
         }
-        for(int i = 0; i < postorder.length; i++) postmap.put(postorder[i], i);
         return buildTree(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
     }
 
@@ -31,13 +27,9 @@ class Solution {
         if(inStart > inEnd || postStart > postEnd) return null;
         TreeNode node = new TreeNode(post[postEnd]);
         int rootIndex = inmap.get(post[postEnd]);
-
-        int rigthIndex = postEnd;
-        for(int i = rootIndex; i <= inEnd; i++) {
-            rigthIndex = Math.min(rigthIndex, postmap.get(in[i]));
-        }
-        node.left = buildTree(in, inStart, rootIndex - 1, post, postStart, rigthIndex - 1);
-        node.right = buildTree(in, rootIndex + 1, inEnd, post, rigthIndex, postEnd - 1);
+        int leftTreeSize = rootIndex - inStart;
+        node.left = buildTree(in, inStart, rootIndex - 1, post, postStart, postStart + leftTreeSize - 1);
+        node.right = buildTree(in, rootIndex + 1, inEnd, post, postStart + leftTreeSize, postEnd - 1);
         return node;
     }
 }
