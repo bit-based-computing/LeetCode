4332b99285045ecaaad3fe3d76fa52ebde434c91
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-06-18 01:30:26 +0600

    Time: 176 ms (5.72%), Space: 63.4 MB (86.81%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,23 +1,29 @@
 class MedianFinder {
-    PriorityQueue<Integer> minHeap;
-    PriorityQueue<Integer> maxHeap;
+    Queue<Integer> queue1;
+    Queue<Integer> queue2;
+    int count = 0;
     public MedianFinder() {
-        minHeap = new PriorityQueue<>((a,b)-> a - b);
-        maxHeap = new PriorityQueue<>((a,b)-> b-a);
+        queue1 = new PriorityQueue<>((a, b) -> b - a);
+        queue2 = new PriorityQueue<>((a, b) -> a - b);
     }
     
     public void addNum(int num) {
-        minHeap.add(num);
-        maxHeap.add(minHeap.poll());
-        if(maxHeap.size() > minHeap.size()) {
-            minHeap.add(maxHeap.poll());
+        count++;
+        queue1.offer(num);
+        if(!queue2.isEmpty()) {
+            queue1.offer(queue2.poll());
+        }
+        while (queue1.size() > queue2.size() + 1) {
+            queue2.offer(queue1.poll());
         }
     }
     
     public double findMedian() {
-        if((minHeap.size() + maxHeap.size()) % 2 == 0)
-            return (minHeap.peek() + maxHeap.peek()) / 2.0; 
-        return minHeap.peek();
+        if(count % 2 == 1) {
+            if(queue2.size() > queue1.size()) return queue2.peek();
+            return queue1.peek();
+        }
+        return (double)(queue1.peek() + queue2.peek())/2.0;
     }
 }
 
