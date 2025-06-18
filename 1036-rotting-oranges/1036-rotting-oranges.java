6168970db3d741cb6a55e4fd0f5ec041babd348d
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-06-18 22:48:53 +0600

    Time: 2 ms (68.41%), Space: 42.8 MB (22.33%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,43 +1,49 @@
 class Solution {
-    int[] dx = {0, -1, 0, 1};
-    int[] dy = {1, 0, -1, 0};
     public int orangesRotting(int[][] grid) {
         int row = grid.length;
         int col = grid[0].length;
-        Queue<List<Integer>> q = new LinkedList<>();
-        for (int i = 0; i < row; i++) {
+        int[][] time = new int[row][col];
+        int dx[] = {1, 0, -1, 0};
+        int dy[] = {0, 1, 0, -1};
+
+        for(int i=0; i < row; i++) {
             for(int j = 0; j < col; j++) {
-                if(grid[i][j] == 2) {
-                    grid[i][j] = -1;
-                    q.add(List.of(i,j));
-                }
+                if(grid[i][j] == 0 || grid[i][j] == 2) time[i][j] = 0;
+                if(grid[i][j] == 1) time[i][j] = -1;
             }
         }
-        int count = -1;
-        while(!q.isEmpty()) {
-            count++;
-            int size = q.size();
-            for(int i = 0; i < size; i++) {
-                List<Integer> l = q.poll();
-                int r = l.get(0);
-                int c = l.get(1);
-                for(int j = 0; j < 4; j++) {
-                    int nr = r + dx[j];
-                    int nc = c + dy[j];
-                    if(nr >=0 && nr < row && nc >=0 && nc < col && grid[nr][nc] == 1) {
-                        grid[nr][nc] = -1;
-                        q.add(List.of(nr,nc));
+
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                if(grid[i][j] == 2) {
+                    Queue<int[]> queue = new LinkedList<>();
+                    queue.add(new int[] {i, j});
+                    while(!queue.isEmpty()) {
+                        int[] x = queue.poll();
+                        for(int k = 0; k < 4; k++) {
+                            int ni = x[0] + dy[k];
+                            int nj = x[1] + dx[k];
+
+                            if(ni >= 0 && nj >= 0 && ni < row && nj < col && grid[ni][nj] == 1) {
+                                if(time[ni][nj] == -1 || time[ni][nj] > time[x[0]][x[1]] + 1) {
+                                    time[ni][nj]  = time[x[0]][x[1]] + 1;
+                                    queue.add(new int[] {ni, nj});
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
-        
-        for (int i = 0; i < row; i++) {
+
+        int ans = 0;
+        for(int i = 0; i < row; i++) {
             for(int j = 0; j < col; j++) {
-                if(grid[i][j] == 1) return -1;
+                if(time[i][j] == -1) return -1;
+                ans = Math.max(ans, time[i][j]);
             }
         }
-        count = Math.max(count, 0);
-        return count;
+
+        return ans;
     }
 }
