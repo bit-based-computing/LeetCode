796582a8a0a52d966c7b3ae4646c523dcab9b1d1
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-06-25 18:19:11 +0600

    Time: 41 ms (58.44%), Space: 44.1 MB (87.09%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,23 +1,17 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int len = nums.length;
-        Integer[][] dp = new Integer[len][len + 1]; 
-        return getAns(-1, 0, nums, dp);
-    }
-
-    public int getAns(int prevIndex, int index, int[] nums, Integer[][] dp) {
-        if (index >= nums.length) return 0;
-
-        if (dp[index][prevIndex + 1] != null) return dp[index][prevIndex + 1];
-
-        int notTake = getAns(prevIndex, index + 1, nums, dp);
-
-        int take = 0;
-        if (prevIndex == -1 || nums[index] > nums[prevIndex]) {
-            take = 1 + getAns(index, index + 1, nums, dp);
+        int[] dp = new int[len];
+        int ans = 1;
+        Arrays.fill(dp, 1);
+        for(int i = 0; i < len; i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            ans = Math.max(ans, dp[i]);
         }
-
-        dp[index][prevIndex + 1] = Math.max(take, notTake);
-        return dp[index][prevIndex + 1];
+        return ans;        
     }
 }
