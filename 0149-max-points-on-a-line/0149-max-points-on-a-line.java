f6e6b1c15affc6fae505e728f55b138f9549255d
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-06-27 23:05:14 +0600

    Time: 49 ms (9.01%), Space: 45.7 MB (5.74%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.java
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -3,26 +3,38 @@ class Solution {
         int len = points.length;
         if (len <= 2)
             return len;
+        int max = 0;
 
-        int max = 2;
         for (int i = 0; i < len; i++) {
-            for (int j = i + 1; j < len; j++) {
-                int count = 2;
-                for (int k = 0; k < len; k++) {
-                    if (k == i || k == j)
-                        continue;
+            Map<String, Integer> slopes = new HashMap<>();
+            int duplicate = 1;
+            int cMax = 0;
+            for (int j = 0; j < len; j++) {
+                if (i == j) continue;
 
-                    boolean isInSameLine = ((points[j][0] - points[i][0])
-                            * (points[k][1] - points[i][1])) == ((points[j][1] - points[i][1])
-                                    * (points[k][0] - points[i][0]));
-
-                    if (isInSameLine)
-                        count++;
+                int y = points[j][1] - points[i][1];
+                int x = points[j][0] - points[i][0];
+                if (x == 0 && y == 0) {
+                    duplicate++;
+                    continue;
                 }
-               max = Math.max(max, count);
-            }
-        }
+                int commonDivisor = gcd(x, y);
+                x = x / commonDivisor;
+                y = y / commonDivisor;
 
+                String s = y + "/" + x;
+                slopes.put(s, slopes.getOrDefault(s, 0) + 1);
+                cMax = Math.max(cMax, slopes.get(s));
+            }
+            
+            max = Math.max(max, cMax + duplicate);
+        }
         return max;
+    }
+
+    public int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
     }
 }
