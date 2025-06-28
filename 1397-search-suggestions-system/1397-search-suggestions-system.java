d0d04feccee2a892822cb5794bc10186437b2fe3
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-06-28 22:54:16 +0600

    Time: 18 ms (70.13%), Space: 48 MB (52.12%) - LeetHub

--- 1397-search-suggestions-system/1397-search-suggestions-system.java
+++ 1397-search-suggestions-system/1397-search-suggestions-system.java
@@ -2,10 +2,11 @@ class Solution {
     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
         Arrays.sort(products);
         TrieNode root = new TrieNode();
+        TrieNode curNode = root;
         for(String product: products) {
             int i = 0;
             int len = product.length();
-            TrieNode curNode = root;
+            curNode = root;
             while(i < len) {
                 char ch = product.charAt(i);
                 int index  = ch - 'a';
@@ -21,22 +22,30 @@ class Solution {
         }
 
         List<List<String>> ans = new ArrayList<>();
+        curNode = root;
+        int start = 0;
         for(int i = 0; i < searchWord.length(); i++) {
-            ans.add(find(0, i, root, searchWord));
+            curNode = find(start, i, curNode, searchWord);
+            start = i + 1;
+            if(curNode == null) {
+                start = 0;
+                curNode = root;
+            }
+            ans.add(curNode.list);
         }
         return ans;
     }
 
-    List<String> find(int start, int end, TrieNode curNode, String searchWord) {
+    TrieNode find(int start, int end, TrieNode curNode, String searchWord) {
             List<String> ans = new ArrayList<>();
             while(start <= end) {
                 char ch = searchWord.charAt(start);
                 int index = ch - 'a';
-                if(curNode.childrens[index] == null) return ans;
+                if(curNode.childrens[index] == null) return null;
                 curNode = curNode.childrens[index];
                 start++;
             }
-            return curNode.list;
+            return curNode;
     }
 }
 
