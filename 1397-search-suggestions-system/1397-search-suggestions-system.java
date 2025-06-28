faefa643de59967d0081c802c8bf255fdc8ce292
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-06-28 22:58:23 +0600

    Time: 16 ms (77.93%), Space: 50.4 MB (21.82%) - LeetHub

--- 1397-search-suggestions-system/1397-search-suggestions-system.java
+++ 1397-search-suggestions-system/1397-search-suggestions-system.java
@@ -2,59 +2,43 @@ class Solution {
     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
         Arrays.sort(products);
         TrieNode root = new TrieNode();
-        TrieNode curNode = root;
-        for(String product: products) {
-            int i = 0;
-            int len = product.length();
-            curNode = root;
-            while(i < len) {
-                char ch = product.charAt(i);
-                int index  = ch - 'a';
-                if(curNode.childrens[index] == null) {
-                    curNode.childrens[index] = new TrieNode();
+
+        for (String product : products) {
+            TrieNode curNode = root;
+            for (char ch : product.toCharArray()) {
+                int index = ch - 'a';
+                if (curNode.children[index] == null) {
+                    curNode.children[index] = new TrieNode();
                 }
-                curNode = curNode.childrens[index];
-                if(curNode.list.size() < 3) {
+                curNode = curNode.children[index];
+                if (curNode.list.size() < 3) {
                     curNode.list.add(product);
                 }
-                i++;
             }
         }
 
         List<List<String>> ans = new ArrayList<>();
-        curNode = root;
-        int start = 0;
-        for(int i = 0; i < searchWord.length(); i++) {
-            curNode = find(start, i, curNode, searchWord);
-            start = i + 1;
-            if(curNode == null) {
-                start = 0;
-                curNode = root;
-            }
-            ans.add(curNode.list);
+        TrieNode curNode = root;
+        for (char ch : searchWord.toCharArray()) {
+            curNode = find(ch, curNode);
+            ans.add(curNode == null ? new ArrayList<>() : curNode.list);
         }
+
         return ans;
     }
 
-    TrieNode find(int start, int end, TrieNode curNode, String searchWord) {
-            List<String> ans = new ArrayList<>();
-            while(start <= end) {
-                char ch = searchWord.charAt(start);
-                int index = ch - 'a';
-                if(curNode.childrens[index] == null) return null;
-                curNode = curNode.childrens[index];
-                start++;
-            }
-            return curNode;
+    TrieNode find(char ch, TrieNode curNode) {
+        if (curNode == null) return null;
+        return curNode.children[ch - 'a'];
     }
 }
 
-class TrieNode{
-    TrieNode[] childrens;
+class TrieNode {
+    TrieNode[] children;
     List<String> list;
-    
+
     TrieNode() {
-        childrens = new TrieNode[26];
+        children = new TrieNode[26];
         list = new ArrayList<>();
     }
 }
