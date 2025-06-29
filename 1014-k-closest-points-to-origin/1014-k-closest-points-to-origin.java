5ddec83f6ab6d59a893916c6590ba09fcd7b2d1c
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-06-29 19:34:32 +0600

    Time: 31 ms (55.66%), Space: 50.7 MB (79.48%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -1,26 +1,24 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a, b) -> Integer.compare(
+                b[0] * b[0] + b[1] * b[1],
+                a[0] * a[0] + a[1] * a[1]
+            )
+        );
 
-        PriorityQueue<double[]> pq = new PriorityQueue<>((double[] a, double[] b) -> Double.compare(b[0], a[0]));
-        for(int[] point: points) {
-            double x = point[0];
-            double y = point[1];
-
-            x = x * x;
-            y = y * y;
-            double dist = Math.sqrt(x + y);
-            pq.add(new double[]{dist, point[0], point[1]});
-            if(pq.size() > k) pq.poll();
+        for (int[] point : points) {
+            pq.offer(point);
+            if (pq.size() > k) {
+                pq.poll();
+            }
         }
 
-        int[][] results = new int[k][2];
-        int i = 0;
-        while(!pq.isEmpty()) {
-            double[] sp = pq.poll();
-            results[i][0] = (int)sp[1];
-            results[i][1] = (int)sp[2];
-            i++;
+        int[][] result = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            result[i] = pq.poll();
         }
-        return results;
+
+        return result;
     }
 }
