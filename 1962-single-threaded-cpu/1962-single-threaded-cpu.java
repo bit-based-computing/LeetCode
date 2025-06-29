0bcb5bdefd230273a7ef5185a71f0ffac870adca
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-06-29 18:23:19 +0600

    Time: 108 ms (45.09%), Space: 100.5 MB (20.16%) - LeetHub

--- 1962-single-threaded-cpu/1962-single-threaded-cpu.java
+++ 1962-single-threaded-cpu/1962-single-threaded-cpu.java
@@ -1,42 +1,39 @@
 class Solution {
     public int[] getOrder(int[][] tasks) {
-        int len = tasks.length;
-        int[] ans = new int[len];
-        Set<Integer> set = new HashSet<>();
-        Map<Integer, List<int[]>> map = new HashMap<>();
+        int n = tasks.length;
+        int[][] sorted = new int[n][3];
 
-        for(int i = 0; i < len; i++) {
-            int inq = tasks[i][0];
-            set.add(inq);
-            if(map.get(inq) == null) map.put(inq, new ArrayList<>());
-            map.get(inq).add(new int[]{i, tasks[i][1]});
+        for (int i = 0; i < n; i++) {
+            sorted[i][0] = tasks[i][0]; // enqueue time
+            sorted[i][1] = tasks[i][1]; // processing time
+            sorted[i][2] = i;           // index
         }
-        List<Integer> list = new ArrayList<>(set);
-        Collections.sort(list);
-        int time = 0;
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((int[] a, int[] b) -> {
-            if(a[1] == b[1]) return Integer.compare(a[0], b[0]);
+        Arrays.sort(sorted, (a, b) -> Integer.compare(a[0], b[0]));
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            if (a[1] == b[1]) return Integer.compare(a[0], b[0]);
             return Integer.compare(a[1], b[1]);
         });
 
-        int i = 0;
-        int j = 0;
+        int time = 0, idx = 0, i = 0;
+        int[] result = new int[n];
 
-        while(j < list.size()) {
-            time = list.get(j);
-            pq.addAll(map.get(list.get(j)));
-            j++;
-            while(!pq.isEmpty()) {
+        while (i < n) {
+            while (idx < n && sorted[idx][0] <= time) {
+                pq.offer(new int[]{sorted[idx][2], sorted[idx][1]});
+                idx++;
+            }
+
+            if (!pq.isEmpty()) {
                 int[] task = pq.poll();
-                ans[i++] = task[0];
+                result[i++] = task[0];
                 time += task[1];
-                while(j < list.size() && list.get(j) <= time) {
-                    pq.addAll(map.get(list.get(j)));
-                    j++;
-                }
+            } else {
+                time = sorted[idx][0];
             }
         }
-        return ans;
+
+        return result;
     }
 }
