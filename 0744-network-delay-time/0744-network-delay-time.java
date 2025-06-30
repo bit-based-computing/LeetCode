ad9f743b696e7dffe28a23ce5a23e468b78d1953
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-06-30 16:19:42 +0600

    Time: 12 ms (61.49%), Space: 49.1 MB (33.98%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.java
+++ 0744-network-delay-time/0744-network-delay-time.java
@@ -5,20 +5,16 @@ class Solution {
             int i = time[0];
             int j = time[1];
             int cost = time[2];
-            if (map.get(i) == null)
-                map.put(i, new ArrayList<>());
+            if (map.get(i) == null)  map.put(i, new ArrayList<>());
             map.get(i).add(new int[] { j, cost });
         }
         int ans = -1;
-        int count = 0;
         int[] cost = new int[n + 1];
         Arrays.fill(cost, Integer.MAX_VALUE);
         PriorityQueue<int[]> queue = new PriorityQueue<>((a,b)-> Integer.compare(a[1], b[1]));
         queue.offer(new int[]{k, 0});
         cost[k] = 0;
         while (!queue.isEmpty()) {
-            int size = queue.size();
-            count += size;
             int[] node = queue.poll();
                 ans = Math.max(ans, cost[node[0]]);
                 if (map.get(node[0]) != null) {
