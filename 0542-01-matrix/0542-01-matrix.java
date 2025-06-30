1ae784d66b01ab2fa64dad073b5eda515d5c63a3
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-06-30 15:31:18 +0600

    Time: 13 ms (85.3%), Space: 47.3 MB (57.97%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,39 +1,37 @@
 class Solution {
     int[] dx = {0, 0, 1, -1};
     int[] dy = {1, -1, 0, 0};
+
     public int[][] updateMatrix(int[][] mat) {
         int row = mat.length;
         int col = mat[0].length;
-
         int[][] visit = new int[row][col];
         Queue<int[]> queue = new LinkedList<>();
+
         for(int i = 0 ; i < row; i++) {
             for(int j = 0; j < col; j++)  {
                 if(mat[i][j] == 1) {
-                    visit[i][j] = -1;
+                    visit[i][j] = -1; // unvisited
                 } else {
-                    queue.offer(new int[]{i,j});
+                    queue.offer(new int[]{i, j}); // start BFS from all 0s
                 }
             }
         }
 
         while(!queue.isEmpty()) {
-            int size = queue.size();
-            for(int i = 0; i < size; i++) {
-                int x = queue.peek()[0];
-                int y = queue.peek()[1];
-                queue.poll();
-                int ans = Integer.MAX_VALUE;
-                for(int k = 0; k < 4; k++) {
-                    int ni = x + dx[k];
-                    int nj = y + dy[k]; 
-                    if(ni >= 0 && nj >= 0 && ni < row && nj < col && visit[ni][nj] == -1) {
-                        visit[ni][nj]= 1 + visit[x][y];
-                        queue.offer(new int[]{ni,nj});
-                    }
+            int[] curr = queue.poll();
+            int x = curr[0], y = curr[1];
+
+            for(int k = 0; k < 4; k++) {
+                int ni = x + dx[k];
+                int nj = y + dy[k];
+                if(ni >= 0 && nj >= 0 && ni < row && nj < col && visit[ni][nj] == -1) {
+                    visit[ni][nj] = visit[x][y] + 1;
+                    queue.offer(new int[]{ni, nj});
                 }
             }
         }
+
         return visit;
     }
 }
