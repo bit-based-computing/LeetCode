81fd5162c61a9be5e6f98b7a1ee7c0aaa07812bf
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-07-01 20:56:46 +0600

    Time: 10 ms (60.69%), Space: 45 MB (24.09%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,14 +1,19 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return way(0, 0, nums, target);
+        int[][] dp = new int[nums.length][2001];
+        for(int[] x: dp) Arrays.fill(x, -1);
+        return way(0, 0, nums, target, dp);
     }
 
-    int way(int sum, int index, int[] nums, int target) {
+    int way(int sum, int index, int[] nums, int target, int[][] dp) {
         if(index > nums.length) return 0;
         if(index == nums.length){
             if(sum == target) return 1;
             return 0;
         }
-        return way(sum + nums[index], index + 1, nums, target) + way(sum - nums[index], index + 1, nums, target);
+        int dpIndex = sum > 0 ? sum : 1000 + Math.abs(sum);
+        if(dp[index][dpIndex] != -1) return dp[index][dpIndex];
+        dp[index][dpIndex] = way(sum + nums[index], index + 1, nums, target, dp) + way(sum - nums[index], index + 1, nums, target, dp);
+        return dp[index][dpIndex];
     }
 }
