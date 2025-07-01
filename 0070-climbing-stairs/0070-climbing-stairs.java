1ca7600cfdf723be86732dc8e80ef9b9e37f6bc5
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-07-01 20:41:49 +0600

    Time: 0 ms (100%), Space: 40.3 MB (81.25%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return way(0, dp, n);
+    }
+
+    int way(int currPos, int[] dp, int n) {
+        if(currPos > n) return 0;
+        if(currPos == n) return 1;
+        if(dp[currPos] != 0-1) return dp[currPos];
+        dp[currPos] = way(currPos + 1, dp, n) + way(currPos + 2, dp, n);
+        return dp[currPos];
+    }
+}
