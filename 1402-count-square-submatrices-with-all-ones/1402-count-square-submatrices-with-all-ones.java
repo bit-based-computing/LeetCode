fee3ecddd23d315fe61e29612c567c0760c54787
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-07-02 22:33:37 +0600

    Time: 6 ms (76.13%), Space: 55 MB (73.05%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.java
@@ -2,38 +2,22 @@ class Solution {
     public int countSquares(int[][] matrix) {
         int row = matrix.length;
         int col = matrix[0].length;
-
+        int[][] dp = new int[row][col];
         int count = 0;
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < col; j++) {
-                if(matrix[i][j] == 1) {
-                    count++;
-                    int ni = i;
-                    int nj = j;
-                    boolean isValid = true;
-                    while(ni + 1 < row && nj + 1 < col) {
-                        ni = ni + 1;
-                        nj = nj + 1;
-                        isValid = true;
 
-                        for(int k = j; k <= nj; k++) {
-                            if(matrix[ni][k] == 0) {
-                                isValid = false;
-                                break;
-                            }
-                        }
-
-                        for(int k = i; k <= ni; k++) {
-                            if(matrix[k][nj] == 0) {
-                                isValid = false;
-                                break;
-                            } 
-                        }
-                        if(!isValid) break;
-                        count++;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == 1) {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(
+                            Math.min(dp[i - 1][j], dp[i][j - 1]),
+                            dp[i - 1][j - 1]
+                        ) + 1;
                     }
+                    count += dp[i][j];
                 }
-                
             }
         }
 
