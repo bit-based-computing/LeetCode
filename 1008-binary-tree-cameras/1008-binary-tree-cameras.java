d25d0f8116fc536c4b5790f524fbba7189e69ef9
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-07-03 23:39:52 +0600

    Time: 0 ms (100%), Space: 42.9 MB (73.86%) - LeetHub

--- 1008-binary-tree-cameras/1008-binary-tree-cameras.java
+++ 1008-binary-tree-cameras/1008-binary-tree-cameras.java
@@ -1,59 +1,30 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    int ans = 0;
+    int cameras = 0;
 
     public int minCameraCover(TreeNode root) {
-        if (root == null)
-            return 0;
-        if (root.left == null && root.right == null)
-            return 1;
-        Map<Integer, int[]> map = new HashMap<>();
-        return countLight(root, 1, 0, map);
+        // If root is not covered, we need an extra camera
+        if (dfs(root) == 0) {
+            cameras++;
+        }
+        return cameras;
     }
 
-    public int countLight(TreeNode root, int node, int light, Map<Integer, int[]> map) {
-        if (root == null)
-            return 0;
-        if(map.get(node) == null) map.put(node, new int[]{-1,-1,-1});
-        int[] dp = map.get(node);
-        if (dp[light] != -1)
-            return dp[light];
-        if (light == 0) {
-            dp[light] = 1 + countLight(root.left, 2 * node, 1, map)
-                    + countLight(root.right, (2 * node) + 1, 1, map);
-            if (root.left != null && root.right != null) {
-                dp[light] = Math.min(dp[light],
-                        countLight(root.left, 2 * node, 2, map) + countLight(root.right, (2 * node) + 1, 0, map));
-                dp[light] = Math.min(dp[light],
-                        countLight(root.left, 2 * node, 0, map) + countLight(root.right, (2 * node) + 1, 2, map));
-            } else if (root.left != null) {
-                dp[light] = Math.min(dp[light], countLight(root.left, 2 * node, 2, map));
-            } else if (root.right != null) {
-                dp[light] = Math.min(dp[light], countLight(root.right, (2 * node) + 1, 2, map));
-            }
-        } else if (light == 1) {
-            dp[light] = Math.min(
-                    1 + countLight(root.left, 2 * node, 1, map) + countLight(root.right, (2 * node) + 1, 1, map),
-                    countLight(root.left, 2 * node, 0, map) + countLight(root.right, (2 * node) + 1, 0, map));
-        } else {
-            dp[light] = 1 + countLight(root.left, 2 * node, 1, map)
-                    + countLight(root.right, (2 * node) + 1, 1, map);
+    // Return states: 0 = not covered, 1 = covered, 2 = has camera
+    private int dfs(TreeNode node) {
+        if (node == null) return 1; // null nodes are covered
+
+        int left = dfs(node.left);
+        int right = dfs(node.right);
+
+        if (left == 0 || right == 0) {
+            cameras++;
+            return 2; // Place a camera here
         }
-        // System.out.println(node + " " + light + " " + dp[node][light]);
-        return dp[light];
+
+        if (left == 2 || right == 2) {
+            return 1; // Covered by child's camera
+        }
+
+        return 0; // Not covered
     }
 }
