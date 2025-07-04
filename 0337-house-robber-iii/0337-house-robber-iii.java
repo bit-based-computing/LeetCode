aa368f23e4dd957a17829228cf61496d4e3c8b12
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-07-04 22:02:20 +0600

    Time: 3 ms (16.76%), Space: 45.5 MB (12.18%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.java
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -15,17 +15,21 @@
  */
 class Solution {
     public int rob(TreeNode root) {
-        int[] money = collectMoney(root);
-        return Math.max(money[0], money[1]);
-    }
-    private int[]  collectMoney(TreeNode root) {
-        if(root == null) return new int[] {0,0};
-        int[] leftSum = collectMoney(root.left);
-        int[] rightSum = collectMoney(root.right);
-        int rob = root.val + leftSum[1] + rightSum[1];
-        int notRob = Math.max(leftSum[1], leftSum[0]) + Math.max(rightSum[0], rightSum[1]);
-        return new int[] {rob, notRob};
-       
+        Map<TreeNode, int[]> map = new HashMap<>();
+        return maxAmount(0, root, map);
     }
 
+    int maxAmount(int prevTaken, TreeNode root,  Map<TreeNode, int[]> map) {
+            if(root ==  null) return 0;
+            if(map.get(root) == null) map.put(root, new int[]{-1,-1});
+            if(map.get(root)[prevTaken] != -1) return map.get(root)[prevTaken];
+            int[] dp = map.get(root);
+            if(prevTaken == 0) {
+                dp[prevTaken] = Math.max(root.val + maxAmount(1, root.left, map) + maxAmount(1, root.right, map), 
+                                maxAmount(0, root.left, map) + maxAmount(0, root.right, map));
+            } else {
+                dp[prevTaken] = maxAmount(0, root.left, map) + maxAmount(0, root.right, map);
+            }
+            return dp[prevTaken];
+    }
 }
