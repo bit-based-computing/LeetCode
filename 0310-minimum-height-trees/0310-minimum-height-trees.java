8e0f73afdc385ea50a5400c8af9332c17993c20a
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-07-06 22:04:57 +0600

    Time: 28 ms (47.97%), Space: 56.8 MB (39.25%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.java
+++ 0310-minimum-height-trees/0310-minimum-height-trees.java
@@ -1,36 +1,33 @@
 class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        for(int i = 0; i < n; i++) {
-            graph.put(i, new ArrayList<>());
-        }
+        if (n == 1) return List.of(0);
 
-        for(int[] edge: edges) {
+        List<Set<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) graph.add(new HashSet<>());
+
+        for (int[] edge : edges) {
             graph.get(edge[0]).add(edge[1]);
             graph.get(edge[1]).add(edge[0]);
         }
 
-        Queue<Integer> leaves = new LinkedList<>();
-        for(int i = 0; i < n; i++) {
-            if(graph.get(i).size() <= 1) {
-                leaves.add(i);
+        List<Integer> leaves = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            if (graph.get(i).size() == 1) leaves.add(i);
+
+        int remainingNodes = n;
+        while (remainingNodes > 2) {
+            remainingNodes -= leaves.size();
+            List<Integer> newLeaves = new ArrayList<>();
+
+            for (int leaf : leaves) {
+                int neighbor = graph.get(leaf).iterator().next();
+                graph.get(neighbor).remove(leaf);
+                if (graph.get(neighbor).size() == 1) newLeaves.add(neighbor);
             }
+
+            leaves = newLeaves;
         }
-        int totalNodes = n;
-        while(totalNodes > 2) {
-            int size = leaves.size();
-            totalNodes -= size;
-            for(int i = 0; i < size; i++) {
-                int leaf = leaves.poll();
-                int neighbor = graph.get(leaf).get(0);
-                graph.get(neighbor).remove((Integer)leaf);
-                if(graph.get(neighbor).size() == 1) leaves.add(neighbor);
-            }
-        }
-        List<Integer> roots = new ArrayList<>();
-        while(!leaves.isEmpty()) {
-            roots.add(leaves.poll());
-        }
-        return roots;
+
+        return leaves;
     }
 }
