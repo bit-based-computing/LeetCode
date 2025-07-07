2fc247d21a0dc382fb8574f6eab53344b92d2c18
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-07-07 12:43:47 +0600

    Time: 39 ms (61.75%), Space: 40.8 MB (76.3%) - LeetHub

--- 1418-fair-distribution-of-cookies/1418-fair-distribution-of-cookies.java
+++ 1418-fair-distribution-of-cookies/1418-fair-distribution-of-cookies.java
@@ -1,28 +1,27 @@
 class Solution {
-    int min;
     public int distributeCookies(int[] cookies, int k) {
-        int[] child = new int[k];
-        min = Integer.MAX_VALUE;
-        dfs(0, cookies, child);
-        return min;
+        int[] childBins = new int[k];
+        // Initial call with a very large unfairness (Infinity) as the starting point
+        return backtrack(cookies, childBins, 0, Integer.MAX_VALUE);
     }
 
-    void dfs(int index, int[] cookies, int[] child) {
-        
-        if(index >= cookies.length) {
-            int max = 0;
-            for(int x: child) {
-                max = Math.max(max, x);
+    private int backtrack(int[] cookies, int[] childBins, int index, int minUnfairness) {
+        if (index == cookies.length) {
+            int currentMax = 0;
+            for (int cookieCount : childBins) {
+                currentMax = Math.max(currentMax, cookieCount);
             }
-            min = Math.min(min, max);
-            return;
+            return Math.min(minUnfairness, currentMax);
         }
-        for(int i = 0; i < child.length; i++) {
-            child[i] += cookies[index];
-            if(child[i] < min) {
-                dfs(index + 1, cookies, child);
+
+        for (int i = 0; i < childBins.length; i++) {
+            childBins[i] += cookies[index];
+
+            if (childBins[i]  < minUnfairness) {
+                minUnfairness = backtrack(cookies, childBins, index + 1, minUnfairness);
             }
-            child[i] -= cookies[index];
+            childBins[i] -= cookies[index];
         }
+        return minUnfairness;
     }
 }
