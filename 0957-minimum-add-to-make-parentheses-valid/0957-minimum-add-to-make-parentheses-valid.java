cfb9a77332d64ad6a5409dbe585ba516914e1755
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-07-09 22:11:26 +0600

    Time: 3 ms (2.51%), Space: 41.4 MB (91.8%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(stack.isEmpty()) {
+                stack.push(c);
+            } else if(c == ')' && stack.peek() == '(') {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.size();
+    }
+}
