56a299a60e739e2c0ba9b1cd2f7b6ff3d8f138e9
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-08-14 00:25:58 +0600

    Time: 168 ms (88.86%), Space: 81.1 MB (11.38%) - LeetHub

--- 2429-design-a-food-rating-system/2429-design-a-food-rating-system.java
+++ 2429-design-a-food-rating-system/2429-design-a-food-rating-system.java
@@ -1,44 +1,87 @@
-class FoodRatings {
-    private Map<String, String> foodToCuisine;
-    private Map<String, Integer> foodToRating;
-    private Map<String, TreeMap<Integer, TreeSet<String>>> cuisineToRatings;
+class Food
+{
+    int rating;
+    String name;
+    String cuisine;
 
-    public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
-        foodToCuisine = new HashMap<>();
-        foodToRating = new HashMap<>();
-        cuisineToRatings = new HashMap<>();
-
-        for (int i = 0; i < foods.length; i++) {
-            String food = foods[i];
-            String cuisine = cuisines[i];
-            int rating = ratings[i];
-
-            foodToCuisine.put(food, cuisine);
-            foodToRating.put(food, rating);
-
-            cuisineToRatings.putIfAbsent(cuisine, new TreeMap<>(Collections.reverseOrder()));
-            cuisineToRatings.get(cuisine).putIfAbsent(rating, new TreeSet<>());
-            cuisineToRatings.get(cuisine).get(rating).add(food);
-        }
-    }
-
-    public void changeRating(String food, int newRating) {
-        String cuisine = foodToCuisine.get(food);
-        int oldRating = foodToRating.get(food);
-
-        TreeMap<Integer, TreeSet<String>> ratingsMap = cuisineToRatings.get(cuisine);
-        TreeSet<String> oldSet = ratingsMap.get(oldRating);
-        oldSet.remove(food);
-        if (oldSet.isEmpty()) ratingsMap.remove(oldRating);
-
-        foodToRating.put(food, newRating);
-        ratingsMap.putIfAbsent(newRating, new TreeSet<>());
-        ratingsMap.get(newRating).add(food);
-    }
-
-    public String highestRated(String cuisine) {
-        TreeMap<Integer, TreeSet<String>> ratingsMap = cuisineToRatings.get(cuisine);
-        int highestRating = ratingsMap.firstKey();
-        return ratingsMap.get(highestRating).first(); // lexicographically smallest food
+    Food(String name, int rating, String cuisine)
+    {
+        this.name = name;
+        this.rating = rating;
+        this.cuisine = cuisine;
     }
 }
+
+class FoodComaparator implements Comparator<Food>
+{
+    @Override
+    public int compare(Food a, Food b)
+    {
+        if(a.rating==b.rating)
+        return a.name.compareTo(b.name);
+        else
+        return b.rating-a.rating;
+    }
+}
+class FoodRatings {
+
+    HashMap<String,Food> map;
+    HashMap<String,PriorityQueue<Food>> map2;
+    
+    public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
+
+        map = new HashMap<String,Food>();
+        map2 = new HashMap<String,PriorityQueue<Food>>();
+
+        for(int i=0;i<foods.length;i++)
+        {
+            Food newNode = new Food(foods[i],ratings[i],cuisines[i]);
+            map.put(foods[i],newNode);
+            if(!map2.containsKey(cuisines[i]))
+            {
+                PriorityQueue<Food> q = new PriorityQueue<Food>(new FoodComaparator());
+                q.add(newNode);
+                map2.put(cuisines[i],q);
+            }
+            else
+            {
+                map2.get(cuisines[i]).add(newNode); 
+            }
+            
+            
+        }
+
+        
+    }
+    
+    public void changeRating(String food, int newRating) {
+        
+        Food temp = map.get(food);
+
+        Food newFood = new Food(food, newRating, temp.cuisine);
+        PriorityQueue<Food> q = map2.get(temp.cuisine);
+
+        //q.remove(temp);
+        //temp.rating =newRating;
+        temp.name ="";
+        q.add(newFood);
+        map.put(food,newFood);
+        
+            
+    }
+    
+    public String highestRated(String cuisine) {
+        PriorityQueue<Food> q = map2.get(cuisine);
+        while(q.peek().name=="")
+        q.poll();
+
+        return q.peek().name;
+    }
+}
+
+/**
+ * Your FoodRatings object will be instantiated and called as such:
+ * FoodRatings obj = new FoodRatings(foods, cuisines, ratings);
+ * obj.changeRating(food,newRating);
+ * String param_2 = obj.highestRated(cuisine);
+ */
