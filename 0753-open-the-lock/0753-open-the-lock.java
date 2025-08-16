5f9dbc618c99d07d1e306f0cf64ff4d0405c7aef
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-08-16 19:47:26 +0600

    Time: 171 ms (12.51%), Space: 53.8 MB (12.67%) - LeetHub

--- 0753-open-the-lock/0753-open-the-lock.java
+++ 0753-open-the-lock/0753-open-the-lock.java
@@ -1,81 +1,36 @@
 class Solution {
     public int openLock(String[] deadends, String target) {
-        Map<String, Integer> map = new HashMap<>();
-        for(int i = 0; i < deadends.length; i++) {
-            map.put(deadends[i], 1);
-        }
+        Set<String> dead = new HashSet<>(Arrays.asList(deadends));
+        if (dead.contains("0000")) return -1;
+
         Queue<String> queue = new LinkedList<>();
-        if(map.get("0000") != null) return -1;
-
         queue.add("0000");
-        map.put("0000", 1);
-        int count = 0;
-        while(!queue.isEmpty()) {
+        Set<String> visited = new HashSet<>();
+        visited.add("0000");
+
+        int steps = 0;
+        while (!queue.isEmpty()) {
             int size = queue.size();
-            // System.out.println(size);
-            for(int i = 0; i < size; i++) {
+            for (int i = 0; i < size; i++) {
                 String curr = queue.poll();
-                if(curr.equals(target)) return count;
-                for(int j = 0; j < 4; j++) {
+                if (curr.equals(target)) return steps;
+
+                for (int j = 0; j < 4; j++) {
                     char c = curr.charAt(j);
-                    int num = Character.getNumericValue(c);
-                    String s1 = "";
-                    String s2 = "";
-                    if(num == 0) {
-                        s1 = curr.substring(0,j);
-                        s1 = s1 + "1" + curr.substring(j+1);
-                        if(map.get(s1) == null) {
-                            map.put(s1, 1);
-                            queue.add(s1);
-                        }
+                    int num = c - '0'; // char → digit
+                    // two neighbors: turn wheel up and down
+                    int[] nextNums = { (num + 1) % 10, (num + 9) % 10 };
 
-                        s2 = curr.substring(0,j);
-                        s2 = s2 + "9" + curr.substring(j+1);
-                        if(map.get(s2) == null) {
-                            map.put(s2, 1);
-                            queue.add(s2);
+                    for (int next : nextNums) {
+                        String nextState = curr.substring(0, j) + next + curr.substring(j + 1);
+                        if (!dead.contains(nextState) && visited.add(nextState)) {
+                            queue.add(nextState);
                         }
-
-                    } else if(num == 9) {
-                        s1 = curr.substring(0,j);
-                        s1 = s1 + "0" + curr.substring(j+1);
-                        if(map.get(s1) == null) {
-                            map.put(s1, 1);
-                            queue.add(s1);
-                        }
-
-                        s2 = curr.substring(0,j);
-                        s2 = s2 + "8" + curr.substring(j+1);
-                        if(map.get(s2) == null) {
-                            map.put(s2, 1);
-                            queue.add(s2);
-                        }
-                    } else {
-                        int num1 = num - 1;
-                        int num2 = num + 1;
-
-                        s1 = curr.substring(0,j);
-                        s1 = s1 + num1 + curr.substring(j+1);
-                        if(map.get(s1) == null) {
-                            map.put(s1, 1);
-                            queue.add(s1);
-                        }
-
-                        s2 = curr.substring(0,j);
-                        s2 = s2 + num2 + curr.substring(j+1);
-                        if(map.get(s2) == null) {
-                            map.put(s2, 1);
-                            queue.add(s2);
-                        }
-
                     }
-                    // System.out.println(s1 + " " + s2);
-
                 }
             }
-            count++;
+            steps++;
         }
-
         return -1;
     }
 }
