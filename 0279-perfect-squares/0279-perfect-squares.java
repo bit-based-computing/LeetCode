c8ad108c2dfa12ba7ddbc34ae037d4f4cbed4223
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-09-03 23:06:48 +0600

    Time: 853 ms (5.04%), Space: 41.3 MB (96.12%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,28 @@
+class Solution {
+    int min = 10001;
+    public int numSquares(int n) {
+        List<Integer> ps = new ArrayList<>();
+        
+        for(int i = 1; i <= n; i++) {
+            int sqr = i * i;
+            if(sqr > n) break;
+            ps.add(sqr);
+        }
+        Collections.sort(ps, Collections.reverseOrder());
+        // System.out.println(ps.size());
+        dfs(0, 0, 0, n, ps);
+        return min;
+    }
+
+    void dfs(int index, int curSum , int count, int n, List<Integer> ps) {
+        if(index >= ps.size()) return;
+        if(count > min) return;
+        if(curSum > n) return;
+        if(curSum == n) {
+            min = Math.min(min, count);
+            return;
+        }
+        dfs(index, curSum + ps.get(index), count +  1, n, ps);
+        dfs(index + 1, curSum , count , n, ps);
+    }
+}
