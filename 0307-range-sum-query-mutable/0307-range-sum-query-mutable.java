78907556b94a68f1539d4b909e2dc9eb633438d7
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-09-04 20:20:02 +0600

    Time: 96 ms (27.75%), Space: 66.3 MB (88.89%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
@@ -1,56 +1,46 @@
 class NumArray {
     Node head;
+
     public NumArray(int[] nums) {
-        int sum = 0;
-        int l = 0;
-        int r = nums.length - 1;
         head = buildTree(nums, 0, nums.length - 1);
     }
-    
+
     public void update(int index, int val) {
         updateTree(head, index, val);
     }
-    
+
     public int sumRange(int left, int right) {
         return getSum(head, left, right);
     }
 
-    public Node buildTree(int[] nums, int l, int r) {
-        if(l == r) return new Node(nums[l], l, r); 
-        Node  node = new Node();
-        int sum = 0;
-        for(int i = l; i <= r; i++) {
-            sum += nums[i];
-        }
-        node.sum = sum;
-        node.l = l;
-        node.r = r;
-        int mid = l + (r - l)/2;
+    private Node buildTree(int[] nums, int l, int r) {
+        if (l == r) return new Node(nums[l], l, r);
+        int mid = l + (r - l) / 2;
+        Node node = new Node(0, l, r);
         node.left = buildTree(nums, l, mid);
-        node.right = buildTree(nums, mid+1, r);
+        node.right = buildTree(nums, mid + 1, r);
+        node.sum = node.left.sum + node.right.sum;
         return node;
     }
 
     public void updateTree(Node node, int index, int val) {
-        if(node.l == index && node.r == index) {
+        if (node.l == node.r) {
             node.sum = val;
             return;
         }
-        if(index <= node.left.r) {
+        if (index <= node.left.r) {
             updateTree(node.left, index, val);
         } else {
             updateTree(node.right, index, val);
-        } 
+        }
         node.sum = node.left.sum + node.right.sum;
     }
 
     public int getSum(Node node, int l, int r) {
-        if(node.l == l && node.r == r) return node.sum;
-        if(r <= node.left.r) {
-            return getSum(node.left, l, r);
-        } else if(l > node.left.r) {
-            return getSum(node.right, l, r);
-        }
+        if (node.l == l && node.r == r) return node.sum;
+        if (r <= node.left.r) return getSum(node.left, l, r);
+        if (l > node.left.r) return getSum(node.right, l, r);
+
         return getSum(node.left, l, node.left.r) + getSum(node.right, node.right.l, r);
     }
 }
@@ -61,7 +51,7 @@ class Node {
     int r = 0;
     Node left;
     Node right;
-    
+
     Node(int sum, int l, int r) {
         this.sum = sum;
         this.l = l;
