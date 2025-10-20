e4b6dc05c4deb03963f3531378f10cfe7aa4791f
Foysal Ahmmed <55128804+bit-based-computing@users.noreply.github.com>
2025-10-20 15:30:35 +0600

    Time: 6 ms (63.52%), Space: 42 MB (94.06%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
@@ -1,35 +1,53 @@
 class Solution {
-    private final int[][] moves = {{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};
+    private static final int[][] DIRECTIONS = {
+        {2, 1}, {1, 2}, {-1, 2}, {-2, 1},
+        {-2, -1}, {-1, -2}, {1, -2}, {2, -1}
+    };
+    
+    private double[][][] memo;
+    private int n;
 
     public double knightProbability(int n, int k, int row, int column) {
-        double[][] dp = new double[n][n];
-        dp[row][column] = 1.0;
-
-        for(int move = 1; move<=k; move++) {
-            double[][] ndp = new double[n][n];
-            for(int r = 0; r<n; r++) {
-                for(int c = 0; c<n; c++) {
-                    for(int[] m: moves) {
-                        int nr = r+m[0];
-                        int nc = c+m[1];
-                        if (isValid(nr, nc, n)) ndp[r][c] += dp[nr][nc]/8.0;
-                    }
+        this.n = n;
+        memo = new double[n][n][k + 1];
+        
+        // Initialize memo with -1 (uncomputed state)
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int t = 0; t <= k; t++) {
+                    memo[i][j][t] = -1.0;
                 }
             }
-            dp = ndp;
+        }
+        
+        return dfs(row, column, k);
+    }
+
+    private double dfs(int r, int c, int k) {
+        // If knight goes out of the board
+        if (r < 0 || r >= n || c < 0 || c >= n) {
+            return 0.0;
+        }
+
+        // Base case: no more moves, still on the board
+        if (k == 0) {
+            return 1.0;
+        }
+
+        // Return memoized value if already computed
+        if (memo[r][c][k] != -1.0) {
+            return memo[r][c][k];
         }
 
         double prob = 0.0;
-        for (int r = 0; r < n; r++) {
-            for (int c = 0; c < n; c++) {
-                prob += dp[r][c];
-            }
+        for (int[] dir : DIRECTIONS) {
+            int nr = r + dir[0];
+            int nc = c + dir[1];
+            prob += dfs(nr, nc, k - 1) / 8.0;
         }
 
+        memo[r][c][k] = prob;
         return prob;
     }
 
-    private boolean isValid(int r, int c, int n) {
-        return r >= 0 && r < n && c >= 0 && c < n;
-    }
 }
